7b5ed5e6ed1971538df684eacc622331f846abe1
carterdeacon <carter.deacon@gmail.com>
2021-11-19 08:31:12 +1000

    Updated top_choices.js for bonus section

--- carter deacon/wk01 - starts 15th Nov/4-thu/top_choices.js
+++ carter deacon/wk01 - starts 15th Nov/4-thu/top_choices.js
@@ -1,13 +1,12 @@
 var topChoices = [];
+var placeArray = ['1st', '2nd', '3rd', '4th', '5th']
 var inputs = 1;
 
 while (inputs < 6) {
     let userInput = prompt("Please enter your favourite color");
     topChoices.push(userInput);
-    console.log(`My #${inputs} choice is ${userInput}.`);
+    console.log(`My ${placeArray[inputs-1]} choice is ${userInput}.`);
     inputs++
 }
 
 console.log(topChoices);
-
-// No idea how to do the bonus part other than manually writing each response
