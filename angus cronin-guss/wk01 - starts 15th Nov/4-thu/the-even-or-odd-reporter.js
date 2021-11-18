23e99a2e7994ec05d6c72f9c5fed19d0f9d98d4c
anguscg <angus.cg@hotmail.com>
2021-11-19 09:26:07 +1100

    Thursday homework

---
+++ angus cronin-guss/wk01 - starts 15th Nov/4-thu/the-even-or-odd-reporter.js
@@ -0,0 +1,26 @@
+var counter = 0; 
+
+while (counter <= 20) { 
+   console.log(counter);
+   counter = counter + 1; 
+
+if (counter % 2 === 0) {
+    console.log("Even");
+} else {
+    console.log("Odd");
+    
+}
+
+}
+
+var nineTimes = 0; 
+
+while (nineTimes <= 10) { 
+    console.log(nineTimes);
+    nineTimes = nineTimes + 1; 
+    console.log(nineTimes * 9);
+    
+}
+
+var num1Choice = ["blue", "orange", "grey", "red", "green"]
+ while 
