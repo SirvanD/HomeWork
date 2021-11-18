cf725d2ea4fbc45a9128393d61149597813e1e9d
SirvanD <sirvansh@gmail.com>
2021-11-18 21:03:31 +1100

    Homework for day4

---
+++ sirvan doukchi/wk01 - starts 15th Nov/4-thu/top_choices.js
@@ -0,0 +1,7 @@
+let favChoices = ['Blue', 'black', 'red', 'grey', 'white'];
+let numbers = ['1st', '2nd', '3rd', '4th', '5th'];
+let counter = 0;
+while (counter <= favChoices.length - 1) {
+    console.log(`My #${numbers[counter]} choice is ${favChoices[counter]}`)
+    counter ++;
+}
