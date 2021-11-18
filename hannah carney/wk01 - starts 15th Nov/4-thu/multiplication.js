7d59f453c7faa6a40da6ef67bde1052b76bfe79d
hannah <hannahcaitlinc@outlook.com>
2021-11-18 22:22:12 +1100

    exercise complete

---
+++ hannah carney/wk01 - starts 15th Nov/4-thu/multiplication.js
@@ -0,0 +1,12 @@
+// Write a while loop that will iterate from 0 to 10. For each iteration of the for loop, it will multiply the number by 9 and log the result (e.g. "2 * 9 = 18").
+
+// 1 x 9 = 9
+// 2 x 9 = 18
+// 3 x 9 = 27
+// 4 x 9 = 36 ...
+
+var number = 0;
+while (number < 90) {
+    console.log(number);
+    number = number + 1 * 9;
+}
