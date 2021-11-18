14000675f2f1c5a111985e93e86788c54dc6626b
Joshua Towney <joshtowney@hotmail.com>
2021-11-18 21:04:51 +1100

    while loops homework complete

---
+++ joshua towney/wk01 - starts 15th Nov/4-thu/odd_even_reporter.js
@@ -0,0 +1,24 @@
+// Write a while loop that will iterate from 0 to 20. For each iteration, it will check if the current number is even or odd, and report that to the screen
+
+// 0 is even
+// 1 is odd
+// 2 is even
+// 3 is odd
+// 4 is even
+// 5 is odd 
+// 6 is even
+// 7 is odd  ...(more lines omitted)
+
+var currentNumber = 0;
+
+while (currentNumber < 21) {
+    
+    if (currentNumber % 2 === 0) {
+    console.log(currentNumber + " is even!");
+    } else {
+        console.log(currentNumber + " is odd!");
+        }
+        currentNumber = currentNumber + 1
+    }
+
+ 
---
+++ joshua towney/wk01 - starts 15th Nov/4-thu/your_top_choices.js
@@ -0,0 +1,24 @@
+// Create an array to hold your top 5 choices (colors, presidents, whatever).
+
+// Using a while loop, for each choice, log to the screen a string like: "My #1 choice is blue."
+
+// Bonus: Change it to log "My 1st choice, "My 2nd choice", "My 3rd choice", picking the right suffix for the number based on what it is.
+
+var bands =['Cog','Karnivool','Periphery','Chevelle','Sevendust'];
+var counter = 0;
+
+while (counter < 5) {
+    if (bands.length === 5) {
+        console.log("My favourite band is " + bands[4]);
+    } else if (bands.length === 4) {
+        console.log("My second favourite band is " + bands[3]);
+    } else if (bands.length === 3) {
+        console.log("My third favourite band is " + bands[2]);
+    } else if (bands.length === 2) {
+        console.log("My forth favoutite band is " + bands[1]);
+    } else {
+        console.log("My fifth favourite band is " + bands[0]);
+    }   
+        bands.pop();
+        counter = counter + 1;
+}
