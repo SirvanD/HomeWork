c82274d3ca1e35fa3531e9e79cf1f0fbb948b463
chando <dave@staticum.net>
2021-11-18 21:12:03 +0800

    Homework - included an index file you can just open to see output in browser console and questions included in the HTML body

---
+++ dave chandler/web-app-demo/loops.js
@@ -0,0 +1,17 @@
+let counter = 1;
+// debugger    
+
+
+
+while (counter <= 20){
+    if (isEven(counter)) {
+        console.log(counter);
+    }
+    counter++;
+}
+
+if(evaluate this content and return either TRUE or FALSE){ 
+    if true - then do this stuff
+} else {
+    do other stuff
+}
