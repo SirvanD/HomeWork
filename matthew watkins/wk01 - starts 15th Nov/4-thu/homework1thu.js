cd221b5f805c1f0ec0ef7950e8a3469dcd145e50
Matthew <mattwatkins000@hotmail.com>
2021-11-18 20:11:45 +1100

    thurs_homework

---
+++ matthew watkins/wk01 - starts 15th Nov/4-thu/homework1thu.js
@@ -0,0 +1,17 @@
+function isEven(value) {
+    return (value%2 == 0);
+    }
+    if (true) {
+    console.log(true) 
+    } else {console.log (false)
+    }
+    //checks to see if value is even & returns true or false
+
+    var num = 0 
+
+    while (num < 20) {
+    if (isEven (num)) {
+        console.log(num)
+    }
+    num = num + 1
+}
