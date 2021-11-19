3efeff5e9960759d3b7c83b7a5218d70f54e76ab
Hughsin23 <hughsy_23@hotmail.com>
2021-11-19 16:38:37 +1100

    forgot to make the movie uppercase

--- hugh sinclair/wk01 - starts 15th Nov/5-fri/checkpoint.js
+++ hugh sinclair/wk01 - starts 15th Nov/5-fri/checkpoint.js
@@ -54,7 +54,7 @@ brain.energyLevel = 99
 var favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 
 var selectedMovieIndex = Math.floor(Math.random() * favMovies.length)
-console.log(favMovies[selectedMovieIndex]);
+console.log(favMovies[selectedMovieIndex].toUpperCase());
 
 
 // prompt('enter a number');
