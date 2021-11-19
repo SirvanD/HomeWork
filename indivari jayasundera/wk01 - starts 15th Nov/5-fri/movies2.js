d576859dbde238ff2666ead8caf60ade3f12b191
indivari <indivarij@gmail.com>
2021-11-19 16:19:03 +1100

    Task 4.1 completed

---
+++ indivari jayasundera/wk01 - starts 15th Nov/5-fri/movies2.js
@@ -0,0 +1,26 @@
+var favMovies = [
+  "Jaws",
+  "The Fellowship of the Ring",
+  "Howl's Moving Castle",
+  "Django Unchained",
+  "Cloud Atlas",
+  "The Usual Suspects",
+  "Toy Story",
+  "Conan the Barbarian",
+  "Titanic",
+  "Harry Potter",
+  "Fried Green Tomatoes",
+  "Volver",
+  "Oculus",
+  "Seven",
+  "Black Panther",
+  "Harry Potter",
+  "Imitation of Life",
+  "Snatch",
+  "Fast and Furious",
+];
+
+var index = Math.floor(Math.random() * favMovies.length);
+
+var randomMovie = favMovies[index];
+console.log(randomMovie);
