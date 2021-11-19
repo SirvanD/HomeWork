d415fe2538cf3c6f9023be06127ab0a2291c05bc
hannah <hannahcaitlinc@outlook.com>
2021-11-19 16:54:03 +1100

    assessment complete

---
+++ hannah carney/wk01 - starts 15th Nov/4-thu/topChoices.js
@@ -0,0 +1,41 @@
+// Create an array to hold your top 5 choices (colors, presidents, whatever).
+
+// Using a while loop, for each choice, log to the screen a string like: "My #1 choice is blue.
+
+
+//  first create an array (nsync members lol)
+
+var favourites = [
+    'Justin',
+    'Lance',
+    'Joey',
+    'JC',
+    'Chris',
+];
+var counter = 0;
+
+//test these in the console at each step
+
+// then use a while loop to output your choices in order 
+
+// the goal is to log every item in order (my first choice is etc)
+
+while (counter < 5) {
+  if (favourites.length = 5) {
+    console.log('my #1 is ' + favourites[0])
+  }
+  else if (favourites.length = 4) {
+    console.log('my #2 is ' + [1])
+  }
+  else if (favourites.length = 3) {
+    console.log('my #2 is ' + [2])
+  }
+  else if (favourites.length = 1) {
+    console.log('my #2 is ' + [3])
+  }
+  else if (favourites.length = 2) {
+    console.log('my #2 is ' + [4])
+  }
+ 
+}
+
---
+++ hannah carney/wk01 - starts 15th Nov/5-fri/assessment.js
@@ -0,0 +1,94 @@
+//////////////////////part 1///////////////////////////
+
+
+// // write code to log maple syrup in the console using the the pantry array above
+
+// var pantry = [
+//     ['nutella', 'honey'],
+//     ['pasta', 'rice', ['flour', 'maple syrup']]
+//   ]
+
+//   console.log(pantry[1][2][1])
+
+//////////////////////part 2///////////////////////////
+
+
+// using a while loop print the following in the console:
+// 17
+// 15
+// 13
+// 11
+// 9
+// 7
+// 5
+// 3
+// 1
+
+// var number = 1;
+// while (number < 18) {
+//     console.log(number);
+//     number = number + 2;
+// }
+
+//////////////////////part 3///////////////////////////
+
+// loop through the days array using a while loop to print the following in the console
+
+// var days = ['Thursday', 'Friday', 'Saturday', 'Sunday']
+// var daysLength = days.length;
+
+// // 0 Thursday
+// // 1 Friday
+// // 2 Saturday
+// // 3 Sunday
+
+// while (days = days.length) {
+//     if (days = daysLength - 1) {
+//         console.log('0 Thursday')
+//     }
+//     if (days = daysLength - 1) {
+//         console.log('1 Friday')
+//     }
+//     if (days = daysLength - 1) {
+//         console.log('2 Saturday')
+//     }
+//     if (days = daysLength - 1) {
+//         console.log('3 Sunday')
+//     }
+// }
+
+//////////////////////part 4///////////////////////////
+
+// var brain = {
+//     energyLevel: 10,
+//     favMovies: ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious']
+//   }
+
+//   write code to console log the last movie using the brain object above (assume Jaws is the first movie)
+// console.log(brain.favMovies[18])
+
+// wirte code to change energyLevel to 99
+// console.log(brain.energyLevel = '99')
+
+
+//////////////////////part 5///////////////////////////
+
+// 
+// var favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+
+// write code to randomly select a movie from the favMovies array and store it in a variable
+// let Generate = Math.floor((Math.random() * favMovies.length));
+// console.log(favMovies[Generate])
+
+// write code to print the movie in uppercase in the console if the randomly selected movie is Jaws
+
+//////////////////////part 6///////////////////////////
+// prompt('enter a number');
+
+// whats the data type of the return value of the above function call
+// an argument
+
+// write code to add 5 to the number user entered and log it on the console
+// var a = Number(prompt("Enter first number"));
+// var b = Number(prompt("Enter second number"));
+// console.log(a + b)
