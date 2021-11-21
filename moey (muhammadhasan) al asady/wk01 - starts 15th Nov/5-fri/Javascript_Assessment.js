f2b09e17daad7ed9025f5d615ae20b25a6f7e125
dembe7 <moey.ozil@gmail.com>
2021-11-21 17:17:25 +1100

    Updated Assessment

--- moey (muhammadhasan) al asady/wk01 - starts 15th Nov/5-fri/Javascript_Assessment.js
+++ moey (muhammadhasan) al asady/wk01 - starts 15th Nov/5-fri/Javascript_Assessment.js
@@ -5,50 +5,49 @@ var pantry = [
     ['pasta', 'rice', ['flour', 'maple syrup']]
   ]
 
-  pantry[0].push('maple syrup')
-
+  pantry[1][2][1]
 
 //   loops (5pt)
 
 // using a while loop print the following in the console:
-// counter = 0;
+num1 = 17;
 
-//THIS IS NOT WORKING I'VE SKIPPED IT
-// counter = 0;
-// function oddNumbers() {
-// while ( counter <= 19) {
-//     counter++;
-// }
-// }
-
-// oddNumbers() ; 
-
-//////////////////////////////////////////////////////////
-
-var days = ['Thursday', 'Friday', 'Saturday', 'Sunday']
-
-while (days.length < 3 ){
-    days.length++;
+while (num1 >= 0) {
+    console.log(num1)
+num1 = num1 - 2;
 }
- console.log(days)
 
-//// OBJECTS AND ARRAYS
+// ////////THIS WORKS, BUT PROBABLY NOT THE RIGHT WAY TO DO IT ///////////////////////////////////////
+
+ var days = ['Thursday', 'Friday', 'Saturday', 'Sunday']
+ var i = -1;
+
+ while( i  < days.length ) {
+   i++;
+   console.log(days[i]) 
+  }
+
+
+
+
+
+// //// OBJECTS AND ARRAYS
  var brain = {
     energyLevel: 10,
     favMovies: ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious']
   }
-  brain.favMovies['18'] ;
+  brain.favMovies.slice(-1);
   brain.energyLevel = 99 ;
 
 
-  //builtin functions (4pt)
+//   //builtin functions (4pt)
   var favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
 // i found this online but it's not working. I've tried the Math.floor but couldnt get it to work. I'll work on this over the weekend. 
-  favMovies.random()
-  favMovies[1].toUpperCase
+var random = favMovies[Math.floor(Math.random()*favMovies.length)];
 
-//   data types and return values (3pt)
+console.log(random.toUpperCase())
+// //   data types and return values (3pt)
 
-var num1 = Number(prompt(enter a number));
-num1 = num1 + '5';
+var num1 = Number(prompt('enter a number'));
+num1 = num1 + Number(5);
 console.log(num1)
---
+++ moey (muhammadhasan) al asady/wk01 - starts 15th Nov/5-fri/hw1.js
@@ -0,0 +1,7 @@
+
+var pantry = [
+    ['nutella', 'honey'],
+    ['pasta', 'rice', ['flour', 'maple syrup']]
+  ]
+
+  pantry[1][2][1]
