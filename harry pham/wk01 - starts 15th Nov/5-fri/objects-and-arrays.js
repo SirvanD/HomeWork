867b3c3163bcedd9bb225db3fca5f19895d36c94
HarryP <harrypham__@hotmail.com>
2021-11-22 09:38:29 +1100

    object and arrays corrected

--- harry pham/wk01 - starts 15th Nov/5-fri/objects-and-arrays.js
+++ harry pham/wk01 - starts 15th Nov/5-fri/objects-and-arrays.js
@@ -4,15 +4,15 @@ var brain = {
     favMovies: ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious']
 }
 
-console.log(brain.favMovies[18]);
+console.log(brain.favMovies[brain.favMovies.length - 1]);
 
 brain.energyLevel = 99
 
-var favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
-var favRandom = Math.floor(Math.random() * favMovies.length);
-favMoviesResult = favMovies[favRandom];
+// var favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+// var favRandom = Math.floor(Math.random() * favMovies.length);
+// favMoviesResult = favMovies[favRandom];
 
-if(favMoviesResult == favMovies[0]){
-    console.log(favMoviesResult.toUpperCase())
+// if(favMoviesResult == favMovies[0]){
+//     console.log(favMoviesResult.toUpperCase())
     
-}
+// }
