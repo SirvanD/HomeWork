21b85a4d7304911dee9dbe667a23759b43148345
anguscg <angus.cg@hotmail.com>
2021-11-23 09:15:31 +1100

    added notes

---
+++ angus cronin-guss/wk02 - starts 22nd Nov/1-mon/homework2.js
@@ -0,0 +1,34 @@
+// Guess the fruit game
+// prompt("Guess the fruit I am thinking of");
+
+var fruits = ["apple", "pinapple", "banana", "blueberry"];
+var randomFruit = Math.floor(Math.random() * 14 + 1);
+  
+
+// NOT MY WORK BELOW. FOUND THIS ONLINE TO HELP ME COMPLETE!!!
+
+    // counting the number of guesses
+    // made for correct Guess
+    //var guess = 1;
+      
+    //document.getElementById("submitguess").onclick = function(){
+      
+   // number guessed by user     
+   //var x = document.getElementById("guessField").value;
+  
+   //if(x == y)
+   //{    
+   //    alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
+               + guess + " GUESS ");
+   //}
+   //else if(x > y) /* if guessed number is greater
+                   than actual number*/ 
+   //{    
+   //    guess++;
+   //    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+   //}
+   //else
+   //{
+  //     guess++;
+  //     alert("OOPS SORRY!! TRY A GREATER NUMBER")
+  // }
