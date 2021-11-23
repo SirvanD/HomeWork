798163624d6211fa3f4f6e1419cf85b3633ab7ae
indivari <indivarij@gmail.com>
2021-11-23 21:44:33 +1100

    Array methods examples done

---
+++ indivari jayasundera/wk02 - starts 22nd Nov/2-tue/javascript/arrays.js
@@ -0,0 +1,11 @@
+var movies = ["Moana", "Onward", "Toy Story", "Inside out"];
+
+movies.push("Frozen");
+movies.pop();
+movies.unshift("Lion King");
+movies.shift();
+console.log(movies.join(" "));
+
+var greeting = "Good morning! Have a nice day";
+var greetingArray = greeting.split(" ");
+console.log(greetingArray);
