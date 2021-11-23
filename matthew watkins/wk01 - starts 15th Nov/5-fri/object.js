55e1e02b2c2fb97ecdf39cff8b80bda348b0a1f1
Matthew <mattwatkins000@hotmail.com>
2021-11-23 18:10:01 +1100

    checkpoint

--- matthew watkins/wk01 - starts 15th Nov/5-fri/object.js
+++ matthew watkins/wk01 - starts 15th Nov/5-fri/object.js
@@ -3,8 +3,8 @@ var brain = {
     favMovies: ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious']
   }
   
-  console.log(brain.favMovies [18])
+  console.log(brain.favMovies[brain.favMovies.length-1])
 
-  console.log(brain.energyLevel = 99)
+  brain.energyLevel = 99
 
   
