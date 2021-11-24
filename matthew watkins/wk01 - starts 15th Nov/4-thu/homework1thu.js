a06872bd7e7d9c77c46ee8908b0fed497766a5fa
Matthew <mattwatkins000@hotmail.com>
2021-11-24 19:46:28 +1100

    homework

--- matthew watkins/wk01 - starts 15th Nov/4-thu/homework1thu.js
+++ matthew watkins/wk01 - starts 15th Nov/4-thu/homework1thu.js
@@ -4,10 +4,10 @@ let ender = -1
 while (starter > ender) {
     if (starter %2 == 0){
 console.log(`${starter} is even`)
-    }else
+    }else 
     console.log(`${starter} is odd`)
-starter --
-}
+starter -- ;
+
     
     //checks to see if value is even & returns true or false
-
+}
