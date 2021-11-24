f28804b41a49fa8090091b8d7b49563b61a3164e
hannah <hannahcaitlinc@outlook.com>
2021-11-24 20:02:12 +1100

    css exercise homework

--- hannah carney/wk02 - starts 22nd Nov/1-mon/fruits-monday.js
+++ hannah carney/wk02 - starts 22nd Nov/1-mon/fruits-monday.js
@@ -18,26 +18,26 @@
 // hint: it's 6 characters long. Starts with b and ends with a. guess the fruit.
 // instead of typing out the hint manually for every fruit. try to use a template string and programmatically work out the starting letter, ending letter and how many characters long.
 
-// var person = prompt("Guess the fruit");
+var person = prompt("Guess the fruit");
 
-// // step 3
-// // Allow the user to guess the fruit repeatedly until they guess correctly. keep track of the number of guesses.
+// step 3
+// Allow the user to guess the fruit repeatedly until they guess correctly. keep track of the number of guesses.
 
 
-// var index = 1; 
+var index = 1; 
 
-// while (index < 6) {
-//   if (person !== computerRandom) {
-//       person = prompt("wrongo, try again. hint: the fruit has " + computerRandom.length + " letters and starts with a " + computerRandom[0] + " and ends with " + computerRandom[computerRandom.length-1]);
-//     } else {
-//     alert('Coolies, you guessed it in ' + index + ' go(es)');
-//     break;
-//   }
-//     index = index + 1; 
-// }
+while (index < 6) {
+  if (person !== computerRandom) {
+      person = prompt("wrongo, try again. hint: the fruit has " + computerRandom.length + " letters and starts with a " + computerRandom[0] + " and ends with " + computerRandom[computerRandom.length-1]);
+    } else {
+    alert('Coolies, you guessed it in ' + index + ' go(es)');
+    break;
+  }
+    index = index + 1; 
+}
 
 // step 4
 // Congratulate the user and display number of attempts they made.
 
 
-// console.log('you guessed correct. It took you' + index + 'attempts to guess correctly') 
+console.log('you guessed correct. It took you' + index + 'attempts to guess correctly') 
