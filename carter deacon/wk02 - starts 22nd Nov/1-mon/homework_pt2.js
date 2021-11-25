43f662896858341b66124b4c2ca47e9556eaa369
carterdeacon <carter.deacon@gmail.com>
2021-11-25 16:15:48 +1000

    Sartres list task complete

--- carter deacon/wk02 - starts 22nd Nov/1-mon/homework_pt2.js
+++ carter deacon/wk02 - starts 22nd Nov/1-mon/homework_pt2.js
@@ -8,7 +8,7 @@ let userInput = prompt("Guess my favourite fruit").toLowerCase;
 let i = 1;
 
 // allowing user 10 guesses as it doesn't need to link to array length in this case given there are other fruits out there.
-while (i < 10) {
+while (userInput !== computerChoice) {
     if (userInput !== computerChoice) {
         userInput = prompt(`Wrong. Try again.
         Hint: It's ${computerChoice.length} letters long. Starts with a ${computerChoice[0]} and ends with a ${computerChoice[computerChoice.length - 1]}`)
