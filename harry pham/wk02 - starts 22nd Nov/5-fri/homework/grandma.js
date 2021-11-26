5c713ccb6da057a996fbb47f987fa3280c8ae0a0
HarryP <harrypham__@hotmail.com>
2021-11-26 18:12:57 +1100

    homework/checkpoint

--- harry pham/wk02 - starts 22nd Nov/5-fri/homework/grandma.js
+++
@@ -1,9 +0,0 @@
-while(promptWord != "BYE"){
-    var promptWord = prompt()
-    if(promptWord == promptWord.toLowerCase()) {
-        console.log("HUH?! SPEAK UP, SONNY")
-    } else {
-        console.log("NO, NOT SINCE 1938!")
-    }
-}
-console.log("goodbye")
