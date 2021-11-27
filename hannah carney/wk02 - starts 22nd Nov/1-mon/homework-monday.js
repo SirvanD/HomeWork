3fed617262782d39b57cde8af47f2e57bd33a15a
hannah <hannahcaitlinc@outlook.com>
2021-11-27 20:52:53 +1100

    homework and checkpoint are in the same html file

--- hannah carney/wk02 - starts 22nd Nov/1-mon/homework-monday.js
+++ hannah carney/wk02 - starts 22nd Nov/1-mon/homework-monday.js
@@ -1,91 +1,91 @@
-// var leakedMembers = ['dt','aden','allison','bern','callum','cath','felipe','jian','jimmy','linda','matt','naveen','ned','peter','sam','sannan','sara','stephen','stuart','tom'];
+var leakedMembers = ['dt','aden','allison','bern','callum','cath','felipe','jian','jimmy','linda','matt','naveen','ned','peter','sam','sannan','sara','stephen','stuart','tom'];
 
-// leakedMembers[0] = 'misty rose';
+leakedMembers[0] = 'misty rose';
 
 // .indexOf - a built in function to get the computer to tell you the index of a certain thing in an array
 
 // .charAt - is a string function
 
-// console.log(leakedMembers.push('ryan','maya')); // adding two names to the list 
+console.log(leakedMembers.push('ryan','maya')); // adding two names to the list 
 
-// console.log(leakedMembers.unshift('dt')); // adding dt to start of the list 
+console.log(leakedMembers.unshift('dt')); // adding dt to start of the list 
 
 
-// let i = 0;
-// // 
-// while (i < leakedMembers.length) {
-//     let leakedMembersSplit = leakedMembers[i].split("");
-//     if (leakedMembersSplit.length == 3|| leakedMembersSplit.length == 5) {
-//     console.log(leakedMembers[i]);
-//     } i++;
-// }
+let i = 0;
+// 
+while (i < leakedMembers.length) {
+    let leakedMembersSplit = leakedMembers[i].split("");
+    if (leakedMembersSplit.length == 3|| leakedMembersSplit.length == 5) {
+    console.log(leakedMembers[i]);
+    } i++;
+}
 
-// var books = [
-//     {
-//     title: 'The Body Keeps Score', author: 'Bessel van der Kolk', alreadyRead: true 
-//     },
+var books = [
+    {
+    title: 'The Body Keeps Score', author: 'Bessel van der Kolk', alreadyRead: true 
+    },
 
-//     {
-//     title: 'The Men Who Stare at Goats', author: 'Jon Ronson', alreadyRead: true
-//     },
+    {
+    title: 'The Men Who Stare at Goats', author: 'Jon Ronson', alreadyRead: true
+    },
 
-//     {
-//     title: 'Awakenings', author: 'Oliver Sacks', alreadyRead: false
-//     },
+    {
+    title: 'Awakenings', author: 'Oliver Sacks', alreadyRead: false
+    },
 
-//     {
-//     title: 'The Ultimate Happiness Prescription', author: 'Deepak Chopra', alreadyRead: true
-//     }
-//     ];
+    {
+    title: 'The Ultimate Happiness Prescription', author: 'Deepak Chopra', alreadyRead: true
+    }
+    ];
 
-// let count = 0; 
-// while (count < books.length) {
-//     if (books[count].alreadyRead == true) {
-//         console.log(`You have read ${books[count].title} by ${books[count].author}`);
-//     } else if (books[count].alreadyRead == false) {
-//         console.log(`You still need to read ${books[count].title} by ${books[count].author}`);
-//     }
-//     count++;
-// }
+let count = 0; 
+while (count < books.length) {
+    if (books[count].alreadyRead == true) {
+        console.log(`You have read ${books[count].title} by ${books[count].author}`);
+    } else if (books[count].alreadyRead == false) {
+        console.log(`You still need to read ${books[count].title} by ${books[count].author}`);
+    }
+    count++;
+}
 
 
 
 // the movie database
 // create an object to store info on your fave movie
 
-// var favMovie = {
-//     name: 'Thor',
-//     duration: 190,
-//     starring: ['Chris Hemsworth', ' Tessa Thompson', ' Taika Waititi', ' and Kate Blanchett']
-// }
+var favMovie = {
+    name: 'Thor',
+    duration: 190,
+    starring: ['Chris Hemsworth', ' Tessa Thompson', ' Taika Waititi', ' and Kate Blanchett']
+}
 
 
-// function theMovie() {
-//     console.log(`${favMovie.name} lasts for ${favMovie.duration} minutes. It stars ${favMovie.starring}`);
-// }
-// theMovie(); 
+function theMovie() {
+    console.log(`${favMovie.name} lasts for ${favMovie.duration} minutes. It stars ${favMovie.starring}`);
+}
+theMovie(); 
 
 
 // Combining words
 
-// function combineWords(one, two) {
-//     return(one+two)
-// }
-// combineWords();
+function combineWords(one, two) {
+    return(one+two)
+}
+combineWords();
 
-//pluralising
-// let number = 1;
-// let noun = 'dog';
+pluralising
+let number = 1;
+let noun = 'dog';
 // needs 2 inputs, a number and a noun 
 
 
-// function pluralize(noun, number) {
-//     if (number > 1) {
-//         return(number +' '+ noun + 's'); 
-//     } else {
-//         return(number + noun)
-//     }
-// }
+function pluralize(noun, number) {
+    if (number > 1) {
+        return(number +' '+ noun + 's'); 
+    } else {
+        return(number + noun)
+    }
+}
     //   return number > 1 ? pluralWord : singularWord; - the short hand for the above
 
 
@@ -95,7 +95,8 @@
 // function wordReverse(str) {
 //     return str.split(" ").reverse(" ").join(" ");
 // }
-// wordReverse("hello");
+
+
 
 //  Find longest word length
 // function findlongestWord(array) {
@@ -115,7 +116,4 @@
 // }
 // findlongestWord(['hannah', 'ren', 'bananaaa']);
 
-//i is index 0, while the length of the array is less than the index of the word, the loop will continue
-
-// what I should improve on: 
-// 
+// i is index 0, while the length of the array is less than the index of the word, the loop will continue
