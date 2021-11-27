ba1f3e5b422e64522e6fd1df225383c4282e0cfb
chando <dave@staticum.net>
2021-11-27 18:08:58 +0800

    yeah i totally didn't read that properly! Soz - this should be correct and also way simpler

--- dave chandler/wk02 - starts 22nd Nov/5-fri/checkpoint.js
+++ dave chandler/wk02 - starts 22nd Nov/5-fri/checkpoint.js
@@ -1,23 +1,12 @@
 let conversation = prompt("what do you want to say to grandma?","speak loudly she's deaf!");
 let isAllUpper = false;
 
-while(!isAllUpper){
-    let i = 0;
-    while(i < conversation.length){
-        console.log(conversation[i], conversation[i].toUpperCase());
-        if(conversation[i] !== conversation[i].toUpperCase()){
-            isAllUpper = false;
-            alert('HUH?! SPEAK UP, SONNY!');
-            conversation = prompt("what do you want to say to grandma?","speak loudly she's deaf!");
-            break;
-        } else{
-            i++;
-            isAllUpper = true;
+while(conversation !== 'BYE'){
+        if(conversation !== conversation.toUpperCase()){
+            alert('HUH?! SPEAK UP, SONNY!');  
+        } else {
+            alert('NO, NOT SINCE 1938!');
         }
-    }
-}
-
-if(isAllUpper==true){
-    alert('NO, NOT SINCE 1938!');
+        conversation = prompt("what do you want to say to grandma?","speak loudly she's deaf!");
 }
 
