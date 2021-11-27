48205d0c47573baf2d8d3998528403ed14a114f3
Joshua Towney <joshtowney@hotmail.com>
2021-11-27 12:53:30 +1100

    grandma homework done

---
+++ joshua towney/wk02 - starts 22nd Nov/5-fri/grandma.js
@@ -0,0 +1,46 @@
+// Write a lovely Grandma program using prompts, alerts and a loop.
+
+// Whatever you say to grandma (whatever you type in), she should respond with
+
+// HUH?! SPEAK UP, SONNY!
+
+// unless you shout it (type in all capitals).
+
+// If you shout, she can hear you (or at least she thinks so) and yells back
+
+// NO, NOT SINCE 1938!
+
+// You can't stop talking to grandma until you shout BYE.
+
+// var hiGrandma = prompt('Say something to Granny');
+
+// while (hiGrandma != 'BYE') {
+//     prompt('say something to granny');
+
+
+
+//     if (hiGrandma === hiGrandma.toUpperCase()) {
+//         console.log("NO, NOT SINCE 1938!")
+//     } else {
+//         console.log("HUH?! SPEAK UP SONNY")
+//     } 
+// }
+// alert("BYE BYE, SONNY")
+
+var hiGrandma = prompt("HI SONNY");
+
+while (hiGrandma != 'BYE') {
+    if (hiGrandma === hiGrandma.toUpperCase()) {
+            alert("NO, NOT SINCE 1938!");
+        } else {
+            hiGrandma = alert("HUH?! SPEAK UP SONNY");
+    }
+    var hiGrandma = prompt('Anything else, dearie?');
+}
+
+alert("BYE BYE, SONNY");
+
+//notes: var hiGrandma = prompt("HI SONNY") needs to be present BEFORE the while loop or it will not be read by the while part of the loop. 
+
+// var areEqual = string1.toUpperCase() === string2.toUpperCase();
+
