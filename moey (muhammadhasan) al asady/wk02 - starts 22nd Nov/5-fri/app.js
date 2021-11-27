edb8dcf0a25e940c9ea70118d90608e59a199cc2
dembe7 <moey.ozil@gmail.com>
2021-11-27 14:03:15 +1100

    ecardly

---
+++ moey (muhammadhasan) al asady/wk02 - starts 22nd Nov/5-fri/app.js
@@ -0,0 +1,12 @@
+
+
+var input1 = prompt('Enter anything');
+
+// for (input1 == "BYE";;){
+if (input1 == input1.toLowerCase) {
+    console.log('HUH?! SPEAK UP, SONNY!')
+} else if (input1 == input1.toUpperCase) {
+    console.log('NO, NOT SINCE 1938!')
+// } console.log('BYE')
+// 
+}
