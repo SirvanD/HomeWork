4b98451e76e01cc67a63dbddf4d883b6841d2a74
Parisaheide <parisaheid85@gmail.com>
2021-11-28 21:32:52 +1100

    completeday5

--- parisa heydarzadeh/wk02 - starts 22nd Nov/5-fri/app.js
+++ parisa heydarzadeh/wk02 - starts 22nd Nov/5-fri/app.js
@@ -1,15 +1,32 @@
 document.querySelector("#sun").textContent = "cheese";
 
+
 document.querySelector("h1").textContent = "do not change any html code";
+
 
 document.querySelector("#mercury").textContent = 5;
 
+
 document.querySelector("#mars").textContent = "20";
 
-document.queryselector(".jupiter")[1].textcontent.style.color = "purple";
+
+document.queryselectorAll(".jupiter")[1].style.color = "purple";
+
+
+function incrementFavNumber (){
+    document.querySelector("#mars")++
+}
+
+document.querySelector(".plus-one-btn").addEventListener("click", incrementFavNumber);
+
 
 document.querySelector(".saturn").value = "sega saturn";
 
 document.querySelector("#pluto").className = "dwarf-planet";
 
-var tomatoColorButton = document.querySelector(".tomatoColorButton");
+function PlutoChangeColor() {
+  document.querySelector("#pluto").style.backgroundColor = "tomato";
+}
+document
+  .querySelector(".tomato-color-btn")
+  .addEventListener("click", PlutoChangeColor);
