485c04749a77e24ba7a2101d6611dae1ce6a189f
hannah <hannahcaitlinc@outlook.com>
2021-11-29 20:36:04 +1100

    atm- im working on this but my docs appt took ages so this is a bit shit

--- hannah carney/wk03 - starts 29th Nov/1-mon/atm.js
+++ hannah carney/wk03 - starts 29th Nov/1-mon/atm.js
@@ -3,9 +3,7 @@ var depositBtn = document.querySelector('.deposit-btn')
 var withdrawBtn = document.querySelector('.withdraw-btn')
 var amount = document.querySelector('.amount')
 
-
 function balance(){
-accountBalance = Number(accountBalance.value)
 }
 
 function deposit(){
@@ -14,7 +12,12 @@ function deposit(){
 
 function withdraw(){
     accountBalance.textContent = Number(accountBalance.textContent) - Number(amount.value)
+    accountBalance = Number(accountBalance.value); 
+    while (accountBalance == 0) {
+       document.querySelector('.withdraw-btn').style.color = mistyrose
+    }
 }
-
 depositBtn.addEventListener('click', deposit)
 withdrawBtn.addEventListener('click', withdraw)
+
+
