91adbe7688b118eda4fb10d433f4f5ec3631e5d6
indivari <indivarij@gmail.com>
2021-11-30 09:22:31 +1100

    GA bank ATM optional extension added

--- indivari jayasundera/wk03 - starts 29th Nov/1-mon/bank.js
+++ indivari jayasundera/wk03 - starts 29th Nov/1-mon/bank.js
@@ -1,11 +1,15 @@
 console.log("Bank app");
-var accBalance = 50;
+var accBalance = 0;
+var withdrawCount = 0;
+var withdrawCharge = 0;
 
 var inputAmt = document.querySelector(".amt-input");
 var depositBtn = document.querySelector(".deposit-btn");
 var withdrawBtn = document.querySelector(".withdraw-btn");
 var balance = document.querySelector(".acc-balance");
+var withdrawFee = document.querySelector(".charge-display");
 balance.value = accBalance;
+withdrawFee.value = withdrawCharge;
 
 function handleDeposit() {
   if (accBalance >= 0) {
@@ -14,13 +18,29 @@ function handleDeposit() {
   accBalance = accBalance + Number(inputAmt.value);
   balance.value = accBalance;
 }
-
+function checkWithdrawLimit() {
+  if (withdrawCount > 10 && withdrawCount < 12) {
+    withdrawCharge += 2;
+    withdrawFee.value = withdrawCharge;
+    withdrawFee.style.backgroundColor = "tomato";
+    console.log("Withdraw charge :" + withdrawCharge);
+    return alert(
+      "Your free withdrawals limit exceeds. There'll be a $2 charge from now on"
+    );
+  } else if (withdrawCount >= 12) {
+    withdrawCharge += 2;
+    withdrawFee.style.backgroundColor = "tomato";
+    withdrawFee.value = withdrawCharge;
+  }
+}
 function handleWithdraw() {
   var bal = accBalance - Number(inputAmt.value);
+  withdrawCount++;
   if (bal < 0) {
     balance.style.backgroundColor = "red";
     alert("Sorry , There's insufficient balance to make your transaction!");
   } else {
+    checkWithdrawLimit();
     accBalance = accBalance - Number(inputAmt.value);
     balance.value = accBalance;
   }
