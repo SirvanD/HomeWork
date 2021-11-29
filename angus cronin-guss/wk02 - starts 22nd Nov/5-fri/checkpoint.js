6a95a9cc3de01f3bcb3a3fff8355d83825f4f618
anguscg <angus.cg@hotmail.com>
2021-11-29 16:39:56 +1100

    friday homework &checkpoint

---
+++ angus cronin-guss/wk02 - starts 22nd Nov/5-fri/checkpoint.js
@@ -0,0 +1,13 @@
+var speak = prompt('Say something to Grandma');
+var shout = speak.toUpperCase();
+var exit = "BYE"
+
+while (speak != shout) {
+    var yell = prompt("HUH?! SPEAK UP, SONNY.");
+     if (yell = yell.toUpperCase()) {
+        var reply = prompt("NO, NOT SINCE 1938");
+    } 
+    if (reply === exit){
+        break
+    }
+}
