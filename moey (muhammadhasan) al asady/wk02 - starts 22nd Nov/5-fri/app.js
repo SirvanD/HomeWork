3a872907ac4acfbb316d6289c9b77d7dab90c5e6
dembe7 <moey.ozil@gmail.com>
2021-11-30 21:55:20 +1100

    atm

--- moey (muhammadhasan) al asady/wk02 - starts 22nd Nov/5-fri/app.js
+++ moey (muhammadhasan) al asady/wk02 - starts 22nd Nov/5-fri/app.js
@@ -1,12 +1,28 @@
 
 
-var input1 = prompt('Enter anything');
 
-// for (input1 == "BYE";;){
-if (input1 == input1.toLowerCase) {
-    console.log('HUH?! SPEAK UP, SONNY!')
-} else if (input1 == input1.toUpperCase) {
-    console.log('NO, NOT SINCE 1938!')
-// } console.log('BYE')
-// 
+
+
+// function grandma (input1) {
+
+
+
+while (input1 != 'BYE'){
+    var input1 = prompt('Enter anything');
+
+    if (input1 == input1.toLowerCase()) {
+        console.log('HUH?! SPEAK UP, SONNY!')
+    }   else {
+        console.log('NO, NOT SINCE 1938!')
+    } 
+
 }
+// } else if (input1 == input1.toUpperCase) {
+//     console.log('NO, NOT SINCE 1938!')
+// // } console.log('BYE')
+// // 
+// }
+
+// }
+
+// grandma('BYE')
