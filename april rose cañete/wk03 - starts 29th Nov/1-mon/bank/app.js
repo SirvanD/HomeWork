a880bcef50317a17a6a0ef77dc20502b802613cc
April Canete <aprilrose.canete93@gmail.com>
2021-11-30 17:40:47 +1100

    added extra js feature

--- april rose cañete/wk03 - starts 29th Nov/1-mon/bank/app.js
+++ april rose cañete/wk03 - starts 29th Nov/1-mon/bank/app.js
@@ -6,35 +6,61 @@
     var depositBtn = document.querySelector('.deposit-btn');
     var numInput = document.querySelector('.number-input');
     var container = document.querySelector('.container');
-
+    var alertMsgBox = document.querySelector('.alert-msg-box');
+    var numOfWithdrawal = 0;
 
     balSpan.textContent = 0;
 
+    function displayAlertMsg() {
+        alertMsgBox.textContent = 'Account charged $2 fee because you exceeded the 10 free withdrawal limit.';
+        alertMsgBox.style.display = 'block';
+        alertMsgBox.style.textDecoration = 'underline';
+    }
 
-    // function bgcChange() {
-    //     container.style.backgroundColor = 'mistyrose';
-    // }
+    function disableAlertMsg() {
+        alertMsgBox.style.display = 'none';
+    }
+
+    function updateBal(val) {
+        return balSpan.textContent = val;
+     }
+    
 
     function withdraw() {
-        var bal = Number(balSpan.textContent) - Number(numInput.value);  
+        var bal = Number(balSpan.textContent) - Number(numInput.value); 
 
         if (Number(numInput.value) > Number(balSpan.textContent)) {
             return 0;
         } else {
-            balSpan.textContent = bal;
+            updateBal(bal);
 
             if (Number(balSpan.textContent) == 0) {
                 balSpan.style.backgroundColor = 'mistyrose';
             } 
         }  
 
-        numInput.value = '';
+        numOfWithdrawal = numOfWithdrawal + 1;
+
+        // extension: check if exceeded the 10 free withdrawal limit - then charge $2 fee
+        if (numOfWithdrawal > 10) {
+            var bal = bal - 2;
+
+            displayAlertMsg();
+
+            updateBal(bal);    
+        }
+
+        alertMsgBox.addEventListener('click', disableAlertMsg);
+
+        numInput.value = '';  
+
+        //console.log('funct withdraw end');
     }
 
     function deposit() {
         var bal = Number(balSpan.textContent) + Number(numInput.value);
 
-        balSpan.textContent = bal;
+        updateBal(bal);
 
         if (Number(balSpan.textContent) > 0) {
             balSpan.style.backgroundColor = 'white';
