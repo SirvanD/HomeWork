cba9f225719919daccc670f3f5049567caf75b70
indivari <indivarij@gmail.com>
2021-11-30 16:49:20 +1100

    Modified Transmogrifier to work without using the built-in pow method

--- indivari jayasundera/wk03 - starts 29th Nov/2-tue/transmogrifier.js
+++ indivari jayasundera/wk03 - starts 29th Nov/2-tue/transmogrifier.js
@@ -1,8 +1,13 @@
 function transmogrifier(num1, num2, num3) {
   var result1 = num1 * num2;
 
-  var result2 = Math.pow(result1, num3);
-  return result2;
+  var pwr = 1;
+  for (let i = 0; i < num3; i++) {
+    pwr = pwr * result1;
+  }
+
+  //var result2 = Math.pow(result1, num3);
+  return pwr;
 }
 
-console.log(transmogrifier(3, 4, 2));
+console.log(transmogrifier(4, 4, 2));
