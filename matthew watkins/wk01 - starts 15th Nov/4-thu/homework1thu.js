ecede445782dc8f19054ab6ee841fe5601415f44
Matthew <mattwatkins000@hotmail.com>
2021-12-01 19:55:31 +1100

    fixed_brackets

--- matthew watkins/wk01 - starts 15th Nov/4-thu/homework1thu.js
+++ matthew watkins/wk01 - starts 15th Nov/4-thu/homework1thu.js
@@ -1,13 +1,13 @@
-let starter = 20
-let ender = -1
+let starter = 0
+let ender = 21
 
-while (starter > ender) {
-    if (starter %2 == 0){
-console.log(`${starter} is even`)
-    }else 
-    console.log(`${starter} is odd`)
-starter -- ;
-
+while (starter < ender ) {
+    if (starter %2 == 0) {
+        console.log(`${starter} is even`)
+    } else {
+        console.log(`${starter} is odd`)
+    }
+    starter ++ ;
     
     //checks to see if value is even & returns true or false
 }
