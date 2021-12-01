f004ea7b2dd46d00b1ab357a4c0c38ec061ac0b3
Matthew <mattwatkins000@hotmail.com>
2021-12-01 17:40:04 +1100

    hello_there

--- matthew watkins/wk03 - starts 29th Nov/1-mon/accountHomework.js
+++ matthew watkins/wk03 - starts 29th Nov/1-mon/accountHomework.js
@@ -2,16 +2,17 @@
 var depositButton = document.querySelector(`.deposit-btn`)
 var withdrawButton = document.querySelector(`.withdraw-btn`)
 var balanceDisplay = document.querySelector(`.balanceno`)
-var balance = 0
+var balance = Number(balanceDisplay.textContent)
 var input = document.querySelector(`.input`)
 
 //savingsNo.textContent = savings
 
 
-
 function handleDeposit() {
     balance = balance + Number(input.value)
     balanceDisplay.textContent = balance
+    if (balance >= 1) {
+        balanceDisplay.style.backgroundColor = ``}
 }
 
 depositButton.addEventListener(`click`, handleDeposit)
@@ -24,13 +25,17 @@ function handleWithdraw() {
     balance = balance - Number(input.value)
     balanceDisplay.textContent = balance
    }
+   if (balance >= 1) {
+    balanceDisplay.style.backgroundColor = ``}
+    if (Number(balanceDisplay.textContent) == 0) {
+        balanceDisplay.style.backgroundColor = `mistyrose`
+    }
+
 }
+
 
 withdrawButton.addEventListener(`click`,handleWithdraw )
 
-if (balance == 0) {
+if (Number(balanceDisplay.textContent) == 0) {
     balanceDisplay.style.backgroundColor = `mistyrose`
-}
-if (balance <= 1) {
-    balanceDisplay.style.backgroundColor = `none`
 }
