5e9d6cdff1bc8b8a504524e8c145fc53e3743689
dembe7 <moey.ozil@gmail.com>
2021-12-01 16:01:04 +1100

    checkpoint 2

--- moey (muhammadhasan) al asady/wk02 - starts 22nd Nov/5-fri/app.js
+++ moey (muhammadhasan) al asady/wk02 - starts 22nd Nov/5-fri/app.js
@@ -10,10 +10,10 @@
 while (input1 != 'BYE'){
     var input1 = prompt('Enter anything');
 
-    if (input1 == input1.toLowerCase()) {
-        console.log('HUH?! SPEAK UP, SONNY!')
-    }   else {
+    if (input1 == input1.toUpperCase() && input1 != 'BYE') {
         console.log('NO, NOT SINCE 1938!')
+    }   else {
+        console.log('HUH?! SPEAK UP, SONNY!')
     } 
 
 }
--- moey (muhammadhasan) al asady/wk03 - starts 29th Nov/2-tue/app.js
+++ moey (muhammadhasan) al asady/wk03 - starts 29th Nov/2-tue/app.js
@@ -1,13 +1,13 @@
-var num1 = 4,
-var num2 = 3,
-var num3 = 2,
+// var num1 = 4,
+// var num2 = 3,
+// var num3 = 2,
 
 
 
 
 
 function transmogrified (num1, num2, num3) {
-    total(num1 + num2) * num3
+    (num1 + num2) * num3
 }
 
 transmogrified (num1, num2, num3);
