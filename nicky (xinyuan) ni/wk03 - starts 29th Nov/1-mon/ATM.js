73e8340ba07331488cc49b79ee123056db877f7c
nicky <nixinyuan0925@gmail.com>
2021-12-02 14:08:28 +1100

    complete

--- nicky (xinyuan) ni/wk03 - starts 29th Nov/1-mon/ATM.js
+++ nicky (xinyuan) ni/wk03 - starts 29th Nov/1-mon/ATM.js
@@ -1,30 +1,32 @@
 console.log("atm");
 
 var numInput = document.querySelector('.num-input');
-var oldBalance = document.querySelector('.old-balance');
-var newBalance = document.querySelector('.new-balance');
+var balance = document.querySelector('.balance');
 var depositBtn = document.querySelector('.deposit-btn');
 var withdrawBtn = document.querySelector('.withdraw-btn');
 var resetBtn = document.querySelector('.reset-btn');
 var cancelBtn = document.querySelector('.cancel-btn');
-
-var oldBalance = 100;
-document.querySelector('.old-balance').textContent = oldBalance;
+var newtranBtn = document.querySelector('.newtran-btn');
 
 function handleDeposit(){
-    var newBalance = oldBalance + Number(numInput.value)
-    document.querySelector('.new-balance').textContent = newBalance;
+    var deposit = Number(numInput.value);
+    var oldBalance = Number(balance.textContent);
+    var newBalance = deposit + oldBalance;
+    document.querySelector('.balance').textContent = newBalance;
     console.log("deposit");
 }
 depositBtn.addEventListener('click',handleDeposit)
 
+
 function handleWithdraw(){
-    var total = oldBalance - Number(numInput.value)
-   if (total < 0){
-        document.querySelector('.new-balance').textContent = "Insufficient balance. Press cancel to restart.";
+    var withdraw = Number(numInput.value);
+    var oldBalance = Number(balance.textContent);
+    var newBalance = oldBalance -withdraw;
+
+   if (newBalance < 0){
+        document.querySelector('h3').textContent = "Insufficient balance. Press cancel to restart.";
     } else {
-        var newBalance = total;
-        document.querySelector('.new-balance').textContent = newBalance;
+        document.querySelector('.balance').textContent = newBalance;
     }
     console.log("withdraw")
 }
@@ -32,20 +34,29 @@ withdrawBtn.addEventListener('click',handleWithdraw)
 
 function handleReset(){
     numInput.value = '';
-    newBalance.textContent = '';
-    document.querySelector('.old-balance').textContent = "?";
+    document.querySelector('h3').textContent = "YOU TRANSACTION IS CONFIRMED";
     console.log("afterTransaction"); 
 }
 resetBtn.addEventListener('click',handleReset)
 
 function handleCancel(){
     numInput.value = '';
-    newBalance.textContent = '';
+    document.querySelector('h3').textContent = "YOUR BALANCE:";
     console.log("cancel"); 
 }
 cancelBtn.addEventListener('click',handleCancel)
 
-//didn't figure out how to let oldBalance equals the last total after clicked 
+function handleNewtran(){
+    numInput.value = '';
+    document.querySelector('h3').textContent = "YOUR BALANCE:";
+    console.log("newtran"); 
+}
+newtranBtn.addEventListener('click',handleNewtran)
+
+
+
+
+
 
 
 
