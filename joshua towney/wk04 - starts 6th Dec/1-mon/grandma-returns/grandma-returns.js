36393292333d963056d50c9438550dc61526e9e0
Joshua Towney <joshtowney@hotmail.com>
2021-12-06 16:47:30 +1100

    Grandma complete

---
+++ joshua towney/wk04 - starts 6th Dec/1-mon/grandma-returns/grandma-returns.js
@@ -0,0 +1,25 @@
+// What if grandma doesn't want you to leave? When you shout BYE, she could pretend not to hear you. Change your previous program so that you have to shout BYE three times in a row. Make sure to test your program: if you shout BYE three times, but not in a row, you should still be talking to grandma.
+
+var hiGrandma = prompt("HI SONNY");
+var byeCounter = 0;
+
+while (byeCounter < 2) {
+    if (hiGrandma == 'BYE') {
+        alert("That's nice dearie");
+        hiGrandma = prompt("So tell me about your girlfriend");
+        byeCounter++;
+    } else if (hiGrandma != 'BYE') {
+        while (hiGrandma != 'BYE') {
+            var years = [1930,1931,1932,1933,1934,1935,1936,1937,1038,1939,1940,1941,1942,1943,1944,1945,1946,1947,1948,1949,1950];
+            let randomYear = Math.floor(Math.random() * years.length)
+            let pickedyear = years[randomYear];
+            if (hiGrandma === hiGrandma.toUpperCase()) {
+                    alert("NO, NOT SINCE " + pickedyear + "!");
+                } else {
+                    hiGrandma = alert("HUH?! SPEAK UP SONNY");
+            }
+            hiGrandma = prompt('Anything else, dearie?');
+        }
+    }
+}
+alert("BYE BYE, SONNY");
