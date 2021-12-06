8ae9bfbc940ac1b9abce3e138eebb45ea716a280
indivari <indivarij@gmail.com>
2021-12-06 16:01:46 +1100

    Vowel count done

---
+++ indivari jayasundera/wk04 - starts 6th Dec/1-mon/vowel.js
@@ -0,0 +1,21 @@
+function vowelCount(word) {
+  let a = word.toLowerCase();
+  const wordArray = a.split("");
+  let countVowels = 0;
+
+  for (let i = 0; i < wordArray.length; i++) {
+    if (
+      wordArray[i] == "a" ||
+      wordArray[i] == "e" ||
+      wordArray[i] == "i" ||
+      wordArray[i] == "o" ||
+      wordArray[i] == "u"
+    ) {
+      countVowels++;
+    }
+  }
+  return countVowels;
+}
+
+console.log(vowelCount("speer"));
+console.log(vowelCount("Spaghettification"));
