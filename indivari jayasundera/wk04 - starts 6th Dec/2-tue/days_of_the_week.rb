069ac559d99052d7a9e2d5b71d1173eee54117fb
indivari <indivarij@gmail.com>
2021-12-07 21:49:32 +1100

    code modified to generate the sorted array

--- indivari jayasundera/wk04 - starts 6th Dec/2-tue/days_of_the_week.rb
+++ indivari jayasundera/wk04 - starts 6th Dec/2-tue/days_of_the_week.rb
@@ -22,5 +22,5 @@ end
 
 #task 5 - Sorting the remaining array
 puts("---------sorted array------------")
-days_of_the_week[0].sort #This doesn't give the correct output when I run the program in the terminal but it does in 'Pry'
-puts days_of_the_week[0]
+sortedArray = days_of_the_week[0].sort 
+puts sortedArray
