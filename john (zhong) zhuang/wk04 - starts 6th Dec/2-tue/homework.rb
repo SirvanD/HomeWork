bf983cfb04ddeb9ab894591512e0cc13e04b0d88
johnzz <zhuangzhong23@gmail.com>
2021-12-07 20:44:01 +1100

    update

--- john (zhong) zhuang/wk04 - starts 6th Dec/2-tue/homework.rb
+++ john (zhong) zhuang/wk04 - starts 6th Dec/2-tue/homework.rb
@@ -7,22 +7,22 @@
 #     - Thursday
 #     - Friday
 #     - Saturday
-days_of_the_week = Array['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+days_of_the_week =['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 
 # ### 2. My calendar says the first day is Monday...
 # - Remove Sunday from the first postion and move it to the last position.  Use array methods.
 days_of_the_week.push(days_of_the_week.shift)
 
 # ### 3. Create a new array of the days of the week:
-# you can do this programmatically or **just type in manually**
+# you can do this programmatically or **just type in manually
 # - The first inner array should be the weekdays
 # - The second inner array should be the weekend days
 
-days_of_the_week = Array['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+days_of_the_week =['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 days_of_the_week.push(days_of_the_week.shift)
 arr1 = days_of_the_week.take(5)
 arr2 = days_of_the_week.drop(5)
-newArr = Array[arr1, arr2]
+newArr = [arr1, arr2]
 p(newArr)
 
 # ### 4. Remove either the weekdays or the weekends
