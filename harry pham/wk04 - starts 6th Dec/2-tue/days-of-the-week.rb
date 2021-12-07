9cb7c32190498fa47ce27ac098e356536d1e188b
HarryP <harrypham__@hotmail.com>
2021-12-07 21:56:05 +1100

    fixed

--- harry pham/wk04 - starts 6th Dec/2-tue/days-of-the-week.rb
+++ harry pham/wk04 - starts 6th Dec/2-tue/days-of-the-week.rb
@@ -10,27 +10,27 @@
 
 
 
-days_of_the_week.shift
-days_of_the_week.push "Sunday"
-puts days_of_the_week
+# days_of_the_week.shift
+# days_of_the_week.push "Sunday"
+# puts days_of_the_week
 
 
-days_of_the_week = [
-    [
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-],
-[
-    "Saturday",
-    "Sunday"
-    ]
-]
+# days_of_the_week = [
+#     [
+#     "Monday",
+#     "Tuesday",
+#     "Wednesday",
+#     "Thursday",
+#     "Friday",
+# ],
+# [
+#     "Saturday",
+#     "Sunday"
+#     ]
+# ]
 
-days_of_the_week.pop
-puts days_of_the_week[0].sort()
+# days_of_the_week.pop
+# puts days_of_the_week[0].sort()
 
 
 
@@ -42,40 +42,40 @@ planeteers[1]
 
 # Add "Heart" to the end of planeteers.
 
-planeteers.push "Heart"
+# planeteers.push "Heart"
 
 # Combine planeteers with rangers = ["Red", "Blue", "Pink", "Yellow", "Black"] and save the result in a variable called heroes.
 rangers = ["Red", "Blue", "Pink", "Yellow", "Black"]
 heroes = planeteers.concat rangers
-puts heroes
+# puts heroes
 
 # Alphabetize the contents of heroes using a method. The Ruby documentation might help.
 
-sort_hero = heroes.sort()
-puts sort_hero
+# sort_hero = heroes.sort()
+# puts sort_hero
 # Randomize the contents of heroes using a method. The Ruby documentation might help.
 
-rand(heroes.length)
+puts heroes.shuffle
 
 # Select a random element from heroes using a method. The Ruby documentation might help.
 
-puts heroes.sample
+# puts heroes.sample
 
 # display each hero in a new line on the terminal using the .each method
 
-number = 1
-heroes.each do |hero|
-puts "#{number} #{hero}"
-number += 1
-end
+# number = 1
+# heroes.each do |hero|
+# puts "#{number} #{hero}"
+# number += 1
+# end
 
 # display each hero in a new line on the terminal using the .each method only if the hero starts with the letter 'W'
 
-number = 1
-heroes.each do |hero|
-    if hero[0] == "W"
-    puts "#{number} #{hero}"
-    number += 1
-    end
+# number = 1
+# heroes.each do |hero|
+#     if hero[0] == "W"
+#     puts "#{number} #{hero}"
+#     number += 1
+#     end
 
-end
+# end
