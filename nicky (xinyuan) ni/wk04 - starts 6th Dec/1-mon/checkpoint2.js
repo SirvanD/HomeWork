f247b17e48cf165c8abd638d2dec1ebf5a3134cc
nicky <nixinyuan0925@gmail.com>
2021-12-07 13:40:22 +1100

    checkpoint2 resubmit'

--- nicky (xinyuan) ni/wk04 - starts 6th Dec/1-mon/checkpoint2.js
+++ nicky (xinyuan) ni/wk04 - starts 6th Dec/1-mon/checkpoint2.js
@@ -1,50 +1,51 @@
 
 //1
-function vowelCount(words){
-    var array =[words].split();
-    console.log(array)
-    var counter =0
-    for(var index =0 ;index<array.length;index++){
-        if( array[index]=="a" || array[index]=="o" || array[index]=="e" || array[index]=="i" || array[index]=="u" || array[index]=="a")
-        counter++
-    }
-    return counter
+function vowelCount(index){
+    var arrayLetter=((index).toLowerCase()).split('');
+    var index = 0;
+    var counter = 0;
+    while(index <= arrayLetter.length){
+        if(arrayLetter[index]=='a' || arrayLetter[index]=='e' || arrayLetter[index]=='o' || arrayLetter[index]=='u' || arrayLetter[index]=='i'){
+            counter++;
+        }
+        index++;
+    } 
+    return(counter);
 }
-
 
 //2
 var userInput = prompt ("Anything you wanna share with grandma?");
-var counter =1 
+var counter = 0;
 
-while(counter <3){
+while(counter < 3){
     var years = Math.floor(Math.random() * 21);
     if(userInput == 'BYE'){
-        counter++ 
+        counter ++ 
         var userInput = prompt ("HUH?! SPEAK UP, SONNY! NOT SINCE " + (years+1930) + "!")
     } else {
+        var counter = 0;
         var userInput = prompt ("HUH?! SPEAK UP, SONNY! NOT SINCE " + (years+1930) + "!")
     }
 }
 
-alert("BYE, SONNY!");
+// alert("BYE, SONNY!");
 
 
-
-//3
-var users = {
-    theresa: {
-      party: "Conserative",
-      favoriteNumbers: [12, 42, 75]
-    },
-    Lord_Buckethead: {
-      party: "Gremloids",
-      favoriteNumbers: [8, 12, 24, 9]
-    },
-    mr_fishfinger: {
-      party: "Independent",
-      favoriteNumbers: [12, 14, 85, 99, 1001]
-    }
-}
-users["theresa"]["party"]
-users["Lord_Buckethead"]["party"]
-users["mr_fishfinger"]["party"]="birds eye";
+// //3
+// var users = {
+//     theresa: {
+//       party: "Conserative",
+//       favoriteNumbers: [12, 42, 75]
+//     },
+//     Lord_Buckethead: {
+//       party: "Gremloids",
+//       favoriteNumbers: [8, 12, 24, 9]
+//     },
+//     mr_fishfinger: {
+//       party: "Independent",
+//       favoriteNumbers: [12, 14, 85, 99, 1001]
+//     }
+// }
+// users["theresa"]["party"]
+// users["Lord_Buckethead"]["party"]
+// users["mr_fishfinger"]["party"]="birds eye";
