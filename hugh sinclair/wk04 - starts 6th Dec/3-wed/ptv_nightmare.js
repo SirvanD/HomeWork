d12c1883c3de607e122cd4070ca5488b7ca53c22
Hughsin23 <hughsy_23@hotmail.com>
2021-12-08 16:06:00 +1100

    done with list for now, moving to planning ptv

---
+++ hugh sinclair/wk04 - starts 6th Dec/3-wed/ptv_nightmare.js
@@ -0,0 +1,20 @@
+var numOfStops = 0
+
+// maybe an array of arrays?
+
+var alamein = ['Flinders Street', 'Richmond', 'East Richmond', 'Burnley', 'Hawthorn', 'Glenferrie']
+
+var glenWaverly = ['Flagstaff', 'Melbourne Central', 'Parliament', 'Richmond', 'Kooyong', 'Tooronga']
+
+var sandringham = ['Southern Cross', 'Richmond', 'South Yarra', 'Prahran', 'Windsor']
+
+function journey(origin, destination){
+    console.log (`origin: ${origin}`)
+    console.log (`destination: ${destination}`)
+
+    
+    
+    
+    
+}
+
