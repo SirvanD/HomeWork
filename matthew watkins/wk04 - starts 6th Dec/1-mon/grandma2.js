697c32d91f45930a4f582db5b07b96c96fcabd6d
Matthew <mattwatkins000@hotmail.com>
2021-12-08 16:23:14 +1100

    homework

--- matthew watkins/wk04 - starts 6th Dec/1-mon/grandma2.js
+++ matthew watkins/wk04 - starts 6th Dec/1-mon/grandma2.js
@@ -1,26 +1,16 @@
-var startPrompt = prompt(`say something to grandma`)
 var byeCounter = 0
 var yearArray = [1930, 1931, 1932, 1933, 1934, 1935, 1936, 1937, 1938, 1939, 1940, 1941, 1942, 1943, 1944, 1945, 1946, 1947, 1948, 1949, 1950]
-var randomYear = Math.floor(Math.random() * yearArray.length)
-var yearInput = yearArray[randomYear]
 
 while (byeCounter != 3) {
-    //startPrompt ;
+    var startPrompt = prompt(`say something to grandma`)
     //guardian statement to see if it is equal to `bye` uppercase
-    if (startPrompt != `BYE`){
-
-    if (startPrompt == startPrompt.toUpperCase()) {
-        startPrompt = prompt(`NO, NOT SINCE ${yearInput}`)
-        byeCounter = 0 ;
-     } else if (startPrompt == startPrompt.toLowerCase())  {
-        startPrompt = prompt(`HUH?! SPEAK UP, SONNY!`)
-        byeCounter = 0
-    } //Conditional if statement to exit your loop if your counter hits three
-     else{
-        startPrompt = `WHAT?, LEAVING SO SOON?`
+    if (startPrompt == `BYE`){
         byeCounter++
-     }
+        alert(startPrompt)
+    } else {
+        var randomYear = Math.floor(Math.random() * yearArray.length)
+        var yearInput = yearArray[randomYear]
+        startPrompt = alert(`NO, NOT SINCE ${yearInput}`)
+        byeCounter = 0
     }
-    //if statement incrementing a counter to 3
-    
 }
