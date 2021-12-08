f314fec133583eabc7dfdc85130fd764f8bc746a
April Canete <aprilrose.canete93@gmail.com>
2021-12-08 16:26:22 +1100

    clean up

--- april rose cañete/wk04 - starts 6th Dec/3-wed/shopping_list_tracker.rb
+++ april rose cañete/wk04 - starts 6th Dec/3-wed/shopping_list_tracker.rb
@@ -6,7 +6,7 @@ require 'pry'
         puts "========="
         puts "1) add new item"
         puts "2) list all items"
-        puts "3) quit"
+        puts "3) quit\n\n"
     end
 
     shopping_list = []
@@ -21,13 +21,17 @@ require 'pry'
         # puts input
         
         if input == 1
+            puts "\n"
             print 'enter item name: '
+        
             shopping_list.push(gets.chomp)
         end
 
         if input == 2
+            puts "\n"
             puts 'you have the following items in your shopping list:'
             puts shopping_list.join(',')
+            puts "\n"
         end
 
         if input == 3
