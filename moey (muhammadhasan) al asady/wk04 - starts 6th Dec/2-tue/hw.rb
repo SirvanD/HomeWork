a8106447eb59695f5e93e5bae89f10564d03c15a
dembe7 <moey.ozil@gmail.com>
2021-12-08 16:49:23 +1100

    HW

--- moey (muhammadhasan) al asady/wk04 - starts 6th Dec/2-tue/hw.rb
+++ moey (muhammadhasan) al asady/wk04 - starts 6th Dec/2-tue/hw.rb
@@ -14,14 +14,19 @@ days_of_the_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Fri
 # 2. My calendar says the first day is Monday...
 # Remove Sunday from the first postion and move it to the last position. Use array methods.
 
-# days_of_the_week.drop('Sunday')
-# days_of_the_week
+days_of_the_week.shift(1)
+days_of_the_week.push("Sunday")
+
 # 3. Create a new array of the days of the week:
 # you can do this programmatically or just type in manually
-
 # The first inner array should be the weekdays
 # The second inner array should be the weekend days
+days_of_the_week = [['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'], ['Saturday','Sunday']]
+
+
 # 4. Remove either the weekdays or the weekends
 # Your choice...
+days_of_the_week.shift()
 
-# 5. Sort the remaining days alphabetically
+# 5. Sort the remaining days alphabeticallyc
+days_of_the_week.sort
