f06979828366b5d8445241e444d21ccba9152a1e
April Canete <aprilrose.canete93@gmail.com>
2021-12-08 16:48:19 +1100

    more code clean up

--- april rose cañete/wk04 - starts 6th Dec/3-wed/shopping_list_tracker.rb
+++ april rose cañete/wk04 - starts 6th Dec/3-wed/shopping_list_tracker.rb
@@ -2,11 +2,7 @@ require 'pry'
 
     
     def menu()
-        puts "main menu"
-        puts "========="
-        puts "1) add new item"
-        puts "2) list all items"
-        puts "3) quit\n\n"
+        puts "main menu \n=========\n1) add new item\n2) list all items\n3) quit\n\n"
     end
 
     shopping_list = []
@@ -21,20 +17,13 @@ require 'pry'
         # puts input
         
         if input == 1
-            puts "\n"
-            print 'enter item name: '
-        
+            print "\nenter item name: "
             shopping_list.push(gets.chomp)
-        end
-
-        if input == 2
+        elsif input == 2
+            puts "\nyou have the following items in your shopping list:"
+            puts shopping_list.join(', ')
             puts "\n"
-            puts 'you have the following items in your shopping list:'
-            puts shopping_list.join(',')
-            puts "\n"
-        end
-
-        if input == 3
+        else 
             break
         end
     end
