a511684067870471a220a61cb42c538ecfd2295c
dembe7 <moey.ozil@gmail.com>
2021-12-08 16:03:39 +1100

    checkpoint

--- moey (muhammadhasan) al asady/wk03 - starts 29th Nov/2-tue/app.js
+++ moey (muhammadhasan) al asady/wk03 - starts 29th Nov/2-tue/app.js
@@ -6,8 +6,70 @@
 
 
 
-function transmogrified (num1, num2, num3) {
-    (num1 + num2) * num3
-}
+// function transmogrified (num1, num2, num3) {
+//     (num1 + num2) * num3
+// }
 
-transmogrified (num1, num2, num3);
+// transmogrified (num1, num2, num3);
+
+
+
+//MAKE THEM GREEN
+
+var box1 = document.querySelector('.box1');
+var box2 = document.querySelector('.box2');
+var box3 = document.querySelector('.box3');
+
+function turnRed(event){
+    var userClick = event.target;
+    console.log(event.target);
+    userClick.disabled = true;
+    userClick.style.backgroundColor = "red";
+
+
+    if (box1.style.backgroundColor == "red" && box2.style.backgroundColor == "red" && box3.style.backgroundColor == "red") {
+        box1.style.backgroundColor = "green";
+        box2.style.backgroundColor = "green";
+        box3.style.backgroundColor = "green";
+    }
+}
+// function turnRedd(event){
+//     var userClick = event.target;
+//     userClick.disabled = true;
+//     box2.style.backgroundColor = "red";
+//     if (box1.style.backgroundColor == "red" && box2.style.backgroundColor == "red" && box3.style.backgroundColor == "red") {
+//         box1.style.backgroundColor = "green";
+//         box2.style.backgroundColor = "green";
+//         box3.style.backgroundColor = "green";
+//     }
+// }
+// function turnReddd(event){
+//     var userClick = event.target;
+//     userClick.disabled = true;
+//     box3.style.backgroundColor = "red";
+//     if (box1.style.backgroundColor == "red" && box2.style.backgroundColor == "red" && box3.style.backgroundColor == "red") {
+//         box1.style.backgroundColor = "green";
+//         box2.style.backgroundColor = "green";
+//         box3.style.backgroundColor = "green";
+//     }
+// }
+
+// function turn
+
+box1.addEventListener('click', turnRed);
+box2.addEventListener('click', turnRed);
+box3.addEventListener('click', turnRed);
+
+
+
+// if (box1.backgroundColor == "red" && box2.backgroundColor == "red" && box3.backgroundColor == "red") {
+//     box1.style.backgroundColor = "green";
+//     box2.style.backgroundColor = "green";
+//     box3.style.backgroundColor = "green";
+// }
+
+
+
+
+
+
---
+++ moey (muhammadhasan) al asady/wk04 - starts 6th Dec/2-tue/hw.rb
@@ -0,0 +1,27 @@
+# 1. Create an array of the days of the week
+# Create a variable named days_of_the_week as an array of the following:
+# Sunday
+# Monday
+# Tuesday
+# Wednesday
+# Thursday
+# Friday
+# Saturday
+
+days_of_the_week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+
+
+# 2. My calendar says the first day is Monday...
+# Remove Sunday from the first postion and move it to the last position. Use array methods.
+
+# days_of_the_week.drop('Sunday')
+# days_of_the_week
+# 3. Create a new array of the days of the week:
+# you can do this programmatically or just type in manually
+
+# The first inner array should be the weekdays
+# The second inner array should be the weekend days
+# 4. Remove either the weekdays or the weekends
+# Your choice...
+
+# 5. Sort the remaining days alphabetically
