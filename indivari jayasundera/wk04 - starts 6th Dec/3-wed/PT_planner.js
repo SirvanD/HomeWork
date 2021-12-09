7938bd9182e9a472445820d44716d365fce0712d
indivari <indivarij@gmail.com>
2021-12-09 20:31:35 +1100

    drafting - backward trip same lineand checking for destination in other lines

--- indivari jayasundera/wk04 - starts 6th Dec/3-wed/PT_planner.js
+++ indivari jayasundera/wk04 - starts 6th Dec/3-wed/PT_planner.js
@@ -24,22 +24,57 @@ const transport = {
   ],
 };
 
+function isDestinationAvailable(des) {
+  if (transport.alameinLine.includes(des)) {
+    return "alamein";
+  } else if (transport.glenwaverleyLine.includes(des)) {
+    return "glen";
+  } else {
+    return "sandrin";
+  }
+}
+
 console.log("Input origin and destination");
 var origin = process.argv[2];
-console.log(origin);
+
 if (transport.alameinLine.includes(origin)) {
   var indexOrigin = transport.alameinLine.indexOf(origin);
   console.log("Origin index: " + indexOrigin);
 }
 
-var destination = process.argv[3];
-console.log(destination);
-if (transport.alameinLine.includes(destination)) {
-  var indexDestination = transport.alameinLine.indexOf(destination);
-  console.log("Destination index: " + indexDestination);
+function printOriginToRichmond(indexOrigin,indexRich){
+  for(let i=indexOrigin; i<= indexRich){
+    console.log()
+  }
 }
 
-for (let i = indexOrigin; i <= indexDestination; i++) {
-  console.log(transport.alameinLine[i]);
-  console.log("--------->");
+var destination = process.argv[3];
+var line ;
+console.log(destination);
+
+if (transport.alameinLine.includes(destination)) {
+  line = 1;
+  var indexDestination = transport.alameinLine.indexOf(destination);
+  console.log("Destination index: " + indexDestination);
+} else if (transport.glenwaverleyLine.includes(destination)){
+  line =2;
+
+}else if (transport.sandringhamLine.includes(destination)){
+  line =3;
+} else //destination not found in a
+{
+  var indexRichmond = transport.alameinLine.indexOf("Richmond");
+}
+
+// backward trip - same line
+if (indexOrigin < indexDestination) {
+  for (let i = indexOrigin; i <= indexDestination; i++) {
+    console.log(transport.alameinLine[i]);
+    console.log("--------->");
+  }
+} else {
+  for (let i = indexOrigin; i >= indexDestination; i--) {
+    console.log(transport.alameinLine[i]);
+    console.log("--------->");
+  }
 }
