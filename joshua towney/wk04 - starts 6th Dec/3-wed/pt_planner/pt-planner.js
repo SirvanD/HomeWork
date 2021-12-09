7a1e9bba938152787fc621989c6cc6211e815a8c
Joshua Towney <joshtowney@hotmail.com>
2021-12-09 16:32:26 +1100

    Thursday homework complete

--- joshua towney/wk04 - starts 6th Dec/3-wed/pt_planner/pt-planner.js
+++ joshua towney/wk04 - starts 6th Dec/3-wed/pt_planner/pt-planner.js
@@ -1,26 +1,73 @@
-var route1 = ['Flinders Street','Richmond','East Richmond','Burnley','Kooyong','Glenferrie']
-var route2 = ['Flinders Street','Richmond','Kooyong','Tooronga']
-var route3 = ['Flinders Street','Richmond','South Yarra','Prahran','Windsor']
-var route4 = ['Flagstaff','Melbourne Central','Parliament','Richmond','Kooyong','Tooronga']
-var route5 = ['Flagstaff','Melbourne Central','Parliament','Richmond','burnley','kooyong','glenferrie']
-var route6 = ['Flagstaff','Melbourne Central','Parliament','Richmond','South Yarra','Prahran','Windsor']
-var route7 = ['Southern Cross','Richmond','South Yarra','Prahran','Windsor']
-var route8 = ['Southern Cross','Richmond','burnley','kooyong','glenferrie']
-var route9 = ['Southern Cross','Richmond','Kooyong','Tooronga']
+//train lines
+// var alamein = ['Flinders Street','Richmond','East Richmond','Burnley','Kooyong','Glenferrie'];
+// var glenWaverly = ['Flagstaff','Melbourne Central','Parliament','Richmond','Kooyong','Tooronga'];
+// var sandringham = ['Southern Cross','Richmond','South Yarra','Prahran','Windsor'];
 
-// function trip (getOn, getOff) {
-    console.log('origin: ' + 'Flinders Street');
-    console.log('destination: ' + 'Glenferrie')
-    var route = [];
+//9 possible routes
+// var route1 = ['Flinders Street','Richmond','East Richmond','Burnley','Kooyong','Glenferrie']
+// var route2 = ['Flinders Street','Richmond','Kooyong','Tooronga']
+// var route3 = ['Flinders Street','Richmond','South Yarra','Prahran','Windsor']
+// var route4 = ['Flagstaff','Melbourne Central','Parliament','Richmond','Kooyong','Tooronga']
+// var route5 = ['Flagstaff','Melbourne Central','Parliament','Richmond','Burnley','Kooyong','Glenferrie']
+// var route6 = ['Flagstaff','Melbourne Central','Parliament','Richmond','South Yarra','Prahran','Windsor']
+// var route7 = ['Southern Cross','Richmond','South Yarra','Prahran','Windsor']
+// var route8 = ['Southern Cross','Richmond','Burnley','Kooyong','Glenferrie']
+// var route9 = ['Southern Cross','Richmond','Kooyong','Tooronga']
 
-    for (i = 0; i < route1.length; i++ ) {
-        route.push(route1[i]);
-        route.push("---->");
+var trainLines = {
+    alamein: ['Flinders Street','Richmond','East Richmond','Burnley','Kooyong','Glenferrie'],
+    glenWaverly: ['Flagstaff','Melbourne Central','Parliament','Richmond','Kooyong','Tooronga'],
+    sandringham: ['Southern Cross','Richmond','South Yarra','Prahran','Windsor']
+}
+
+//indexOf() can find the position of the stop on the array
+//toUpperCase/lowercase to ensure the word is correct either way
+//booleans needed to know if we can reach destination from origin on same track
+//includes() can work this out
+
+function trip(getOn) {
+
+    var originToLower = getOn.toLowerCase();
+    var words = originToLower.split(" ");
+    var wordsCapitalized = [];
+
+    for (let i = 0; i < words.length; i++) {
+        wordsCapitalized.push(words[i][0].toUpperCase() + words[i].slice(1));
+        console.log(wordsCapitalized);
     }
-    route.pop();
+    var trueGetOn = wordsCapitalized.join(" ");
 
-    var numberOfStops = Maths.floor(route.length/2);
-    
-    console.log(route.join(' '))
-    console.log(numberOfStops + " stops total")
+    if (trainLines.alamein.includes(trueGetOn)) {
+        console.log("Start on Alemain line");
+    } else if (trainLines.glenWaverly.includes(trueGetOn)) {
+        console.log("Start on Glen Waverly Line");
+    } else if (trainLines.sandringham.includes(trueGetOn)) {
+        console.log('Start on Sandringham line');
+    } else {
+        console.log("Not a station. Goodbye");
+    }
+} 
+
+
+
+
+// function trip(getOn, getOff) {
+//     var route = [];
+
+//     for (i = route1.indexOf(getOn); i <= route1.indexOf(getOff) + 1; i++ ) {
+//         route.push(route1[i]);
+//     }
+//     route.pop();
+//     if (route[0] == undefined) {
+//         route.shift()
+//     }
+//     console.log(route);
+
+//     var numberOfStops = (route.length - 1);
+//     var mappedRoute = route.join(' ---> ')
+
+//     console.log('origin: ' + getOn);
+//     console.log('destination: ' + getOff);
+//     console.log(mappedRoute);
+//     console.log(numberOfStops + " stops total")
 // } 
---
+++ joshua towney/wk04 - starts 6th Dec/4-thu/ruby_hashes.rb
@@ -0,0 +1,50 @@
+require 'pry'
+
+res = {
+  data: {
+    success: true,
+    cards: [
+        {
+          image: "https://deckofcardsapi.com/static/img/KH.png",
+          value: "KING",
+          suit: "HEARTS",
+          code: "KH"
+        },
+        {
+          image: "https://deckofcardsapi.com/static/img/8C.png",
+          value: "8",
+          suit: "CLUBS",
+          code: "8C"
+        },
+        {
+          image: "https://deckofcardsapi.com/static/img/4S.png",
+          images: {
+            svg: "https://deckofcardsapi.com/static/img/4S.svg",
+            png: "https://deckofcardsapi.com/static/img/4S.png"
+          },
+          value: "4",
+          suit: "SPADES",
+          code: "4S",
+        },
+        {
+          image: "https://deckofcardsapi.com/static/img/QH.png",
+          value: "QUEEN",
+          suit: "HEARTS",
+          code: "QH"
+        }
+    ],
+    deck_id: "3p40paa87x90",
+    remaining: 50
+  }
+}
+
+# binding.pry
+# 1: write code to print in the terminal the svg image url for the 4 of spades
+
+puts res[:data][:cards][2][:images][:svg]
+
+# 2: iterate over all the cards to print in the terminal the following:
+puts "#{res[:data][:cards][0][:value]} of #{res[:data][:cards][0][:suit]}"
+puts "#{res[:data][:cards][1][:value]} of #{res[:data][:cards][1][:suit]}"
+puts "#{res[:data][:cards][2][:value]} of #{res[:data][:cards][2][:suit]}"
+puts "#{res[:data][:cards][3][:value]} of #{res[:data][:cards][3][:suit]}"
