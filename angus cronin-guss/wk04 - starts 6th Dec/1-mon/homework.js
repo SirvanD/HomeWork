cad04b2be2970189986377ad03e878603aa6f022
anguscg <angus.cg@hotmail.com>
2021-12-10 17:09:01 +1100

    q1 and q2 revised

--- angus cronin-guss/wk04 - starts 6th Dec/1-mon/homework.js
+++ angus cronin-guss/wk04 - starts 6th Dec/1-mon/homework.js
@@ -1,44 +1,32 @@
 // Question 1 (Vowel Count)
-
-var vowels = ["a", "e", "i", "o", "u"]
-
 function vowelCount(str) {
-    let count = 0;
-    for (let letter of str.toLowerCase()) {
-        if (vowels.includes(letter)) {
-            count++;
-        }
-    } return count;
+  var vowelList = 'aeiou'
+  var countedVowels = 0;
+  
+  for(var i = 0; i < str.length ; i++) {
+    if (vowelList.indexOf(str[i]) !== -1) {
+       countedVowels += 1;
+    }
+  } return countedVowels;
 }
 
 //Question 2 (Grandma)
-// var speak = prompt("Say something to Grandma");
-// var randomYear = Math.ceil(Math.random() * (1950 - 1930 + 1) + 1930);
+var speak = prompt("Say something to Grandma");
+var byeCounter = 0;
+while (byeCounter !== 3) {
+  if (speak === 'BYE') {
+    speak = prompt('What?')
+    byeCounter = byeCounter + 1;
+  } else if (speak === speak.toUpperCase()) {
+      speak = prompt("No, not since " + Math.floor(Math.random() * (1950 - 1930 + 1) + 1930))
+  } else if (byeCounter === 3) {
+      alert('You have left Grandma')
+  } else {
+        speak = prompt("HUH?! SPEAK UP, SONNY");
+    }
+} 
 
-// function firstLoop() {
-//     while (speak !== 'BYE') {
-//         if (speak === speak.toUpperCase()) {
-//             speak = prompt(`No, not since ${randomYear}!`);
-//         } else if (speak == "BYE") {
-//             prompt(secondLoop());
-//         } else {
-//             speak = prompt("HUH?! SPEAK UP, SONNY");
-//         }
-//     }
-// } 
-// firstLoop();
 
-// function secondLoop() {
-//     while (speak == "BYE") {
-//        speak1 = prompt("Pardon?");
-//     } if (speak1 == "BYE") {
-//         speak2 = prompt('PARDON?');
-//     } else if (speak2 == "BYE") {
-//         alert('You have left grandma')
-//     } else {
-//         firstLoop();
-//     }
-// }
 // Question 3 
 var users = {
     theresa: {
