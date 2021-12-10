c7a2fa11a6e06d8b8a19d2a7fd386a9cc0cba8bb
Joshua Towney <joshtowney@hotmail.com>
2021-12-11 10:37:55 +1100

    movies 1 homework complete

--- joshua towney/wk04 - starts 6th Dec/5-fri/movies/main.rb
+++ joshua towney/wk04 - starts 6th Dec/5-fri/movies/main.rb
@@ -1,6 +1,6 @@
-require 'sinatra'
-require 'sinatra/reloader'
-require 'httparty'
+require 'sinatra' #lets up develop web app
+require 'sinatra/reloader' #reloads the page automatically so that we don;t need to reset the server 
+require 'httparty' #so we can access movie database
 
 
 get '/' do
@@ -16,14 +16,20 @@ get'/about' do
 end
 
 get '/movie_details' do
-    "Moive poster goes here"
-    movie = params["movie"]
-    url = "https://omdbapi.com/?t=#{movie}&apikey=ebc931bb"
+    movie = params["movie"] #params[""] gets thrown into the url at the selected spot to give a complete url when searching a movie
+    url = "https://omdbapi.com/?t=#{movie}&apikey=ebc931bb" #url variable. DOUBLE QUOTES needed. Paramter set up after = as #{}
 
-    res = HTTParty.get(url)
-    puts res["Title"]
-    puts res["Year"]
-    puts res["Plot"]    
-    erb(:movie_details)
+    res = HTTParty.get(url) #variable. Url is dependent on the inut above
+
+    title = res["Title"] #setting up variable names for easy access later
+    year = res["Year"]
+    poster = res["Poster"]
+
+    erb(:movie_details, locals: {  #erb :movie_details links to the movie_details erb file once we are on /movie_details
+                        #locals lets us use the below variables on that page when caling in crab claws
+        title: title,
+        year: year,
+        poster: poster
+    })
 
 end
