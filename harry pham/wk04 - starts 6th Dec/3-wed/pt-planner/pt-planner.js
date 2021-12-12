c806b9be4d709000a2924cc22b409666ba33030f
HarryP <harrypham__@hotmail.com>
2021-12-13 03:03:13 +1100

    pt planner

--- harry pham/wk04 - starts 6th Dec/3-wed/pt-planner/pt-planner.js
+++ harry pham/wk04 - starts 6th Dec/3-wed/pt-planner/pt-planner.js
@@ -1,116 +1,129 @@
 var originName = document.querySelector("#origin_name").value
 var destinationName = document.querySelector("#destination_name").value
-var stationOne = document.querySelector(".train1")
-var stationTwo = document.querySelector(".train2")
-var stationThree = document.querySelector(".train3")
+var initialRoute = document.querySelector(".train1")
+var finalRoute = document.querySelector(".train2")
 var totalStops = document.querySelector(".total-stops")
 var routeBtn = document.querySelector(".search")
 var originOptions = document.querySelectorAll("#origin_name")
 var destinationOptions = document.querySelectorAll("#destination_name")
 var resetBtn = document.querySelector(".reset")
 
-
-var transport = [
+var trainLine = [
     alamein= ["Flinders Street", "Richmond", "East Richmond", "Burnley", "Hawthorn", "Glenferrie"],
     glenWaverly= ["Flagstaff", "Melbourne Central", "Parliament", "Richmond", "Kooyong", "Tooronga"],
     sandringhan= ["Southern Cross", "Richmond", "South Yarra", "Prahran", "Windsor"]
 ]
 
-let firstStation = []
-let secondStation = []
-let thirdStation = []
-let test1 = ""
-let test2 = ""
-let test3 = ""
+/*
+Flinder Street -> Kooyong = ["Flinder Street", "Richmond", "Kooyong"]
+Flinder street -> Melbourne central = ["Flinder Street", "Richmond", "Parliament", "Melbourne Central"]
+Burnley -> Kooyong = ["Burnley", "Eash Richmond", "Richmond", "Kooyong"]
 
+Flinder Street -> Prahran = ["Flinder Street", "Richmond", "South Yarra", "Prahran"]
+Hawthorn -> Southern Cross = ["Hawthorn", "Burnley", "East Richmond", "Richmond", "Southern Cross"]
+South Yarra -> Flinder Street = ["South Yarra", "Richmond", "Flinder Street"]
+find distance between origin and richmond, then find distance between richmond and destination
+*/
+
+let initialStation = []
+let finalStation = []
+let thirdStation = []
+let initialRouting = ""
+let finalRouting = ""
+let test3 = ""
 var origin = ""
-console.log("origin", origin)
 var destination = ""
-console.log("destination", destination)
+var originTrainLine = 0
+var originIndex = 0
+var destinationTrainLine = 0
+var destinationIndex = 0
 
 function originSelect(event) {
     let originSelected = event.target
-    console.log(originSelected)
     origin = originSelected.value
-    console.log(origin)
-    
 }
-
+// can join
 function destinationSelect(event) {
     let destinationSelected = event.target
     destination = destinationSelected.value
-    console.log(destination)
+    console.log(event)
 }
 
-function checkRoutes() {
-    for(var i=0; i < transport.length; i++) {
-        if(transport[i].includes(origin)) {
-            console.log("transport location number " + i + " and array number " + transport[i].indexOf(origin))
-            var inputOrigin = transport[i].indexOf(origin)
-        }
-    }
-
-    for(var i=0; i < transport.length; i++) {
-        if(transport[i].includes(destination)) {
-            if(i === 0 && transport[0].includes(origin)) {
-                console.log("transport # ",transport[i].indexOf(destination))
-                var inputDestination = transport[i].indexOf(destination)
-                firstStation = transport[0].slice(inputOrigin, (inputDestination + 1))
-            } else if (i === 1 && transport[0].includes(origin) && origin != "Richmond") {
-                var inputDestination = transport[i].indexOf(destination) - 2
-                firstStation = transport[0].slice(inputOrigin, (transport[0].indexOf("Richmond") + 1))
-                secondStation = transport[1].slice(transport[1].indexOf("Richmond"), (inputDestination + 3))
-            } else if (i === 1 && transport[1].includes(origin)) {
-                var inputDestination = transport[i].indexOf(destination)  
-                secondStation = transport[1].slice(inputOrigin, (inputDestination + 1))
-            } else if (i === 2 && transport[0].includes(origin) && origin != "Richmond") {
-                var inputDestination = transport[i].indexOf(destination)
-                firstStation = transport[0].slice(inputOrigin, (transport[0].indexOf("Richmond") + 1))
-                secondStation = transport[1].slice(transport[1].indexOf("Richmond"), 4)
-                thirdStation = transport[2].slice(transport[2].indexOf("Richmond"), (inputDestination + 1))
-            } else if (i === 2 && transport[1].includes(origin) && origin != "Richmond") {
-                var inputDestination = transport[i].indexOf(destination) + 2
-                secondStation = transport[1].slice(inputOrigin, (transport[0].indexOf("Richmond") + 1))
-                thirdStation = transport[2].slice(transport[2].indexOf("Richmond"), (inputDestination + 1))
-            } else if (i ===2 && transport[2].includes(origin)) {
-                var inputDestination = transport[i].indexOf(destination)
-                thirdStation = transport[2].slice(inputOrigin, (inputDestination + 1))
+function setPoint(type, station) {
+    console.log(station)
+    console.log(trainLine)
+    for(var i=0; i < trainLine.length; i++) {
+        if(trainLine[i].includes(station)) {
+            if (type == "origin") {
+                originTrainLine = i
+                originIndex = trainLine[i].indexOf(station)
+            }
+            if (type == "destination") {
+                destinationTrainLine = i
+                destinationIndex = trainLine[i].indexOf(station)
             }
         }
     }
+}
 
-    originName.textContent = origin
-    console.log("input origin ", inputOrigin)
-    destinationName.textContent = destination
-    console.log("input destination ", inputDestination)
+function checkRoutes() {
+    setPoint("origin", origin)
+    setPoint("destination", destination)
 
+    console.log("origin Train Line", originTrainLine)
+    console.log("destination Train Line", destinationTrainLine)
+    if(originTrainLine === destinationTrainLine) {
+        if(destinationIndex < originIndex) {
+            var reversed = trainLine[originTrainLine].reverse()
+            initialStation = reversed.slice(trainLine[originTrainLine].indexOf(origin), trainLine[originTrainLine].indexOf(destination) + 1)
+        } else {
+            initialStation = trainLine[originTrainLine].slice(originIndex, destinationIndex + 1)
+        }
+    } else {
+        if(originTrainLine == 0 && originIndex > 1 || originTrainLine == 1 && originIndex > 3 || originTrainLine == 2 && originIndex > 1) {
+            var reversed = trainLine[originTrainLine].reverse()
+            initialStation = reversed.slice(trainLine[originTrainLine].indexOf(origin), trainLine[originTrainLine].indexOf("Richmond") + 1)
+        } else {
+            initialStation = trainLine[originTrainLine].slice(originIndex, trainLine[originTrainLine].indexOf("Richmond") + 1)
+            console.log(initialStation)
+            // finalStation = trainLine[destinationTrainLine].slice(trainLine[destinationTrainLine].indexOf("Richmond"), destinationIndex + 1)
+            console.log(finalStation)
+        }
+        if(destinationTrainLine == 0 && destinationIndex > 1 || destinationTrainLine == 1 && destinationIndex > 3 || destinationTrainLine == 2 && destinationIndex > 1) {
+            finalStation = trainLine[destinationTrainLine].slice(trainLine[destinationTrainLine].indexOf("Richmond"), destinationIndex + 1)
+        } else {
+            var reversed = trainLine[destinationTrainLine].reverse()
+            finalStation = reversed.slice(trainLine[destinationTrainLine].indexOf("Richmond"), trainLine[destinationTrainLine].indexOf(destination) + 1)
+        }
+    }
+    console.log("initial station", initialStation)
+    console.log("final station", finalStation)
+    initialRouting = initialStation.join(" =====> ")
+    finalRouting = finalStation.join(" =====> ")
 
-    test1 = firstStation.join(" =====> ")
-    console.log("test1", test1)
-    console.log("firstStation", firstStation)
-    test2 = secondStation.join(" =====> ")
-    console.log("test2", test2)
-    console.log("secondStation", secondStation)
-    test3 = thirdStation.join(" =====> ")
-    console.log("test3", test3)
-    console.log("thirdStation", thirdStation)
+    if(initialStation.length !== 1) {
+        initialRoute.textContent = initialRouting
 
+    }
+    if(finalStation.length !== 1) {
+        finalRoute.textContent = finalRouting
+    }
 
-    stationOne.textContent = test1
-    stationTwo.textContent = test2
-    stationThree.textContent = test3
-
-
-    var count = inputDestination - inputOrigin 
-    console.log("total stops", count)
-    totalStops.textContent = count
+    if(originTrainLine === destinationTrainLine) {
+        var count =  finalStation.length + initialStation.length - 1
+        totalStops.textContent = count
+    } else {
+        var count =  finalStation.length + initialStation.length - 2
+        totalStops.textContent = count
+    }
+    routeBtn.disabled = true
 }
 
 function handleReset() {
-    stationOne.textContent = ""
-    stationTwo.textContent = ""
-    stationThree.textContent = ""
+    initialRoute.textContent = ""
+    finalRoute.textContent = ""
     totalStops.textContent = ""
+    routeBtn.disabled = false
 }
 
 routeBtn.addEventListener("click", checkRoutes)
@@ -118,7 +131,7 @@ originOptions.forEach(function(origins) {
     origins.addEventListener("input", originSelect)
 })
 destinationOptions.forEach(function(destinations) {
-    destinations.addEventListener("change", destinationSelect)
+    destinations.addEventListener("input", destinationSelect)
 })
 
 resetBtn.addEventListener("click", handleReset)
