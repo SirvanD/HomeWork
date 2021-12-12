8e28e54e4d668f6b4866f6b951f404ea0d035930
anguscg <angus.cg@hotmail.com>
2021-12-12 22:10:32 +1100

    reupload

---
+++ angus cronin-guss/wk04 - starts 6th Dec/4-thu/hashes.rb
@@ -0,0 +1,39 @@
+res = {
+  data: {
+    success: true,
+    cards: [
+        {
+          image: "https://deckofcardsapi.com/static/img/KH.png",
+          value: "KING",
+          suit: "HEARTS",
+          code: "KH"
+        },
+        {
+          image: "https://deckofcardsapi.com/static/img/8C.png",
+          value: "8",
+          suit: "CLUBS",
+          code: "8C"
+        },
+        {
+          image: "https://deckofcardsapi.com/static/img/4S.png",
+          images: {
+            svg: "https://deckofcardsapi.com/static/img/4S.svg",
+            png: "https://deckofcardsapi.com/static/img/4S.png"
+          },
+          value: "4",
+          suit: "SPADES",
+          code: "4S",
+        },
+        {
+          image: "https://deckofcardsapi.com/static/img/QH.png",
+          value: "QUEEN",
+          suit: "HEARTS",
+          code: "QH"
+        }
+    ],
+    deck_id: "3p40paa87x90",
+    remaining: 50
+  }
+}
+
+puts 
