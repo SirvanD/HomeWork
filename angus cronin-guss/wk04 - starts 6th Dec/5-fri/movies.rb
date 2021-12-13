11d8b29efc7e7c862e0faf560d76c0b211c9de83
anguscg <angus.cg@hotmail.com>
2021-12-13 18:33:03 +1100

    Q1 complete, still working on Q2

--- angus cronin-guss/wk04 - starts 6th Dec/5-fri/movies.rb
+++ angus cronin-guss/wk04 - starts 6th Dec/5-fri/movies.rb
@@ -2,6 +2,10 @@ require 'httparty'
 require 'sinatra'
 require 'sinatra/reloader'
 
+url = 'http://www.omdbapi.com/?i=tt3896198&apikey=ffba863'
+
+res = HTTParty.get(url)
+
 get '/' do 
     erb(:index, locals:{
 
