23368c9646b056e80847c30520133c3bde2e0b78
SirvanD <sirvansh@gmail.com>
2021-12-13 21:15:11 +1100

    HomeWork-13decmon

--- sirvan doukchi/wk04 - starts 6th Dec/5-fri/main.rb
+++ sirvan doukchi/wk04 - starts 6th Dec/5-fri/main.rb
@@ -1,6 +1,8 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty' 
+require 'stock_quote'
+StockQuote::Stock.new(api_key: "pk_5297b0e5a91b4f75afc2b8a65438b6f8")
 
 get '/' do 
 
@@ -31,4 +33,51 @@ get '/movie_details' do
         movie_poster: poster
     })
 
+    
+
 end
+
+
+get '/stock'  do
+    
+    
+    ticker = params["ticker"].to_s
+    stock = StockQuote::Stock.quote(ticker)
+
+    company_name = stock.company_name
+    price = stock.latest_price
+    "Stock of #{company_name} is last sold at US $ #{price}"
+
+    # erb(:stock_details, locals: {
+    #     company_name: company_name,
+    #     price: price
+    # })
+
+end
+
+get '/popular_stocks' do
+    tickers = [
+        ['msft', 'Microsoft'],
+        ['tsla', 'Tesla'],
+        ['aapl', 'Apple'],
+        ['googl', 'Alphabet Inc'],
+        ['nflx', 'Netflix Inc']
+    ]
+
+    erb(:popular_stocks, locals: {
+        tickers: tickers
+    })
+
+
+
+    # html = ""
+    # tickers.each do |ticker|
+    #     html += "<p> <a href =''>#{ticker} </a> </p>"
+    # end
+
+    # return html
+
+    
+    
+    # "Hello are the popular stocks"
+  end
