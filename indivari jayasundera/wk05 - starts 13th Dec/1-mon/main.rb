0f1d4826016259ca4333a2e00b3167631bbdaeda
indivari <indivarij@gmail.com>
2021-12-13 21:19:59 +1100

    Movie details displayed when clicked on a link

--- indivari jayasundera/wk05 - starts 13th Dec/1-mon/main.rb
+++ indivari jayasundera/wk05 - starts 13th Dec/1-mon/main.rb
@@ -7,6 +7,22 @@ get '/' do
     erb(:index, { :layout => :layout })
 end
 
+get '/movie_details' do 
+    movie = params["movie"]
+    url = "https://omdbapi.com/?t=#{movie}&apikey=2f6435d9"
+    res = HTTParty.get(url)
+    
+    title = res["Title"]
+    year = res ["Year"]
+    poster = res ["Poster"]
+   
+    erb(:movie_details, locals: {
+        title:title,
+        year:year,
+        poster:poster
+    })
+end
+
 get '/movie_list' do 
     movie = params["movie"]
     url = "https://omdbapi.com/?s=#{movie}&apikey=2f6435d9"
