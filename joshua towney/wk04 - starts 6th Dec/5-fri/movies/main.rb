e07649228b7f21fb392f235388bdf6df4e65c297
Joshua Towney <joshtowney@hotmail.com>
2021-12-13 22:44:38 +1100

    Homework 70% Complete

--- joshua towney/wk04 - starts 6th Dec/5-fri/movies/main.rb
+++ joshua towney/wk04 - starts 6th Dec/5-fri/movies/main.rb
@@ -1,6 +1,6 @@
-require 'sinatra' #lets up develop web app
-require 'sinatra/reloader' #reloads the page automatically so that we don;t need to reset the server 
-require 'httparty' #so we can access movie database
+require 'sinatra' # what runs the Sinatra web framework that allows us to receive requests and provide responses.
+require 'sinatra/reloader' # doesn't reload the page automatically, instead it reloads the code in the main.rb file every time a page refresh happens.
+require 'httparty' # yes it allows us to access movie database, but more specifically httparty allows us to make a request to any web address. We happen to use it to make a request to the OMDB API website to retrieve JSON data about a movie.
 
 
 get '/' do
@@ -16,8 +16,8 @@ get'/about' do
 end
 
 get '/movie_details' do
-    movie = params["movie"] #params[""] gets thrown into the url at the selected spot to give a complete url when searching a movie
-    url = "https://omdbapi.com/?t=#{movie}&apikey=ebc931bb" #url variable. DOUBLE QUOTES needed. Paramter set up after = as #{}
+    movie = params["movie"] #params[""] gets thrown into the url at the selected spot to give a complete url when searching a movie KASUN accesses the value inside a query parameter named movie in the web url.
+    url = "https://omdbapi.com/?t=#{movie}&apikey=ebc931bb" #url variable. DOUBLE QUOTES needed. Parameter set up after = as #{} KASUN - double quotes needed cause we're using string interpolation to embed the movie variable inside the string.
 
     res = HTTParty.get(url) #variable. Url is dependent on the inut above
 
---
+++ joshua towney/wk05 - starts 13th Dec/1-mon/movies2/main.rb
@@ -0,0 +1,43 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'httparty'
+
+get '/' do 
+    "MOVIE DATABASE MAGIC"
+    erb(:index)
+end
+
+get '/about' do
+    "ABOUT"
+    erb(:about)
+end
+
+get '/all_matches' do
+    movie = params["movie"]
+    url_list = "https://omdbapi.com/?s=#{movie}&apikey=ebc931bb"
+    all_matches = HTTParty.get(url_list)
+    search = all_matches["Search"]
+
+    erb(:all_matches, locals: {
+        all_matches: all_matches,
+        search: search,
+    })
+end
+
+get '/movie_details' do
+    "MOVIE DETAILS"
+
+    movie = params["movie"]
+    url = "https://omdbapi.com/?t=#{movie}&apikey=ebc931bb"
+    selected_movie = HTTParty.get(url)
+    title = selected_movie["Title"]
+    year = selected_movie["Year"]
+    poster = selected_movie["Poster"]
+
+    erb(:movie_details, locals: {
+        title: title,
+        year: year,
+        poster: poster
+    })
+
+end
