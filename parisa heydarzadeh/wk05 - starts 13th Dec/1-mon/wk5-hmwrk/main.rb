783ca9c5170056928af066559980b2defa5e9c5e
Parisaheide <parisaheid85@gmail.com>
2021-12-14 16:30:07 +1100

    commit

---
+++ parisa heydarzadeh/wk05 - starts 13th Dec/1-mon/wk5-hmwrk/main.rb
@@ -0,0 +1,41 @@
+require 'httparty'
+require 'sinatra'
+require 'sinatra/reloader'
+
+
+get '/' do
+    erb(:index)
+end
+
+    get '/movie_details' do
+        movie_title=params["apple"]
+    
+        url = "http://www.omdbapi.com/?t=#{movie_title}&apikey=890ff11d"
+        res = HTTParty.get(url)
+        puts res
+    
+    erb(:movie_details, locals: {
+        movie_title: res["Title"],
+        movie_year: res["Year"],
+        movie_image: res["Poster"]
+    })
+    end
+
+get '/movie_list' do
+
+
+    movie_search = params["movie_search"]
+    url = "https://www.omdbapi.com/?s=#{movie_search}&apikey=890ff11d"
+    res = HTTParty.get(url)
+    title = res["Title"],
+    movie_search = res["Search"],
+    search = movie_search[0]
+
+    erb(:movie_list, locals: {
+        movie_title: title,
+        movie_list: movie_search,
+        search: search,
+        title: title
+        
+    })
+
---
+++ parisa heydarzadeh/wk05 - starts 13th Dec/2-tue/checkpoint/app.rb
@@ -0,0 +1,7 @@
+def letter_reverse(str) do
+    str_Array= str.split('')
+    str_Array == str_Array.reverse 
+
+    
+end
+puts "Now I know what a TV dinner feels like"
