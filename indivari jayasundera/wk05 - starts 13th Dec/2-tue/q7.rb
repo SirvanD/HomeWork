c4c6797635119b376563400c3eacb5388eae3e55
indivari <indivarij@gmail.com>
2021-12-14 16:15:30 +1100

    Q7 code improved

--- indivari jayasundera/wk05 - starts 13th Dec/2-tue/q7.rb
+++ indivari jayasundera/wk05 - starts 13th Dec/2-tue/q7.rb
@@ -2,16 +2,15 @@ str = "Now I know what a TV dinner feels like"
 
 def letter_reverse(str)
     array =  str.split(" ")
-    reverse_phrase =""
     reverse_array= []
 
     array.each do |word|
    reverse_word = word.reverse
 
     reverse_array.push(reverse_word)
- end
+    end
 
-return reverse_array.join(" ")
+    return reverse_array.join(" ")
 end
 
 puts letter_reverse(str)
