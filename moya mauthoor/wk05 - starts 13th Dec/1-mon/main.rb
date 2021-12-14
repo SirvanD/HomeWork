266a6b8836cc919b039b5973da36519ff120668f
moya <moyamauthoor@outlook.com>
2021-12-14 17:15:48 +1100

    Worked on the first extension task a bit and is now working but missing the plot details. Will have to revisit to fix

--- moya mauthoor/wk05 - starts 13th Dec/1-mon/main.rb
+++ moya mauthoor/wk05 - starts 13th Dec/1-mon/main.rb
@@ -13,25 +13,21 @@ get "/search" do
     res = HTTParty.get(url)
     movie_list = res["Search"]
 
-    # Currently working on extension task but might have to come back to it as my brain is melting
-    # if movie_list.length == 1 
-    #     movieID = movie_list[0]["imdbID"]
-    #     movieID = params["id"]
-    #     url = "https://omdbapi.com/?i=#{ movieID }&apikey=8f66dc3c#"
-    #     res = HTTParty.get(url)
-    
-    #     erb(:movie, locals: {
-    #         title: res["Title"],
-    #         year: res["Year"],
-    #         plot: res["Plot"],
-    #         poster: res["Poster"]
-    #     })
-    # else 
+    if movie_list.length.to_i == 1 
+        movie = movie_list[0]
+
+        erb(:movie, locals: {
+        title: movie["Title"],
+        year: movie["Year"],
+        plot: movie["Plot"],
+        poster: movie["Poster"]
+        })
+    else 
         erb(:search_results, locals: {
         movie_list: movie_list
     })
         
-    # end
+    end
 
 end
 
