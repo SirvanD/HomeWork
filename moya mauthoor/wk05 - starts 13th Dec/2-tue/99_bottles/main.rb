f57d2538f13fbded73f076c7683502fcfe9cd134
moya <moyamauthoor@outlook.com>
2021-12-15 16:58:41 +1100

    Database homework complete, answers included in the basic queries document

--- moya mauthoor/wk05 - starts 13th Dec/2-tue/99_bottles/main.rb
+++ moya mauthoor/wk05 - starts 13th Dec/2-tue/99_bottles/main.rb
@@ -8,21 +8,34 @@ get "/" do
   })
 end
 
-get "/:number" do
-  number = params["number"]
-  if number.to_i == 0
-    erb(:no_bottles)
-  elsif number.to_i == 1
-    erb(:bottles, locals: {
-      number: number,
+# Refactored to have separate get routes for 0 and 1
+get "/0" do
+  erb(:no_bottles)
+end
+
+get "/1" do
+  erb(:bottles, locals: {
+      number: 1,
       bottle_or_bottles: "bottle"
     })
-  else
+end
+
+get "/:number" do
+  number = params["number"].to_i
+  # Original if/else statement
+  # if number.to_i == 0
+  #   erb(:no_bottles)
+  # elsif number.to_i == 1
+  #   erb(:bottles, locals: {
+  #     number: number,
+  #     bottle_or_bottles: "bottle"
+  #   })
+  # else
     erb(:bottles, locals: {
       number: number,
       bottle_or_bottles: "bottles"
     })
-  end
+  # end
 end
 
 
