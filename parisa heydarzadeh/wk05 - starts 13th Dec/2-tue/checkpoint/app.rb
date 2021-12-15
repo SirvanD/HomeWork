339a73e606815e7ed81ab8f79aa103d6788318ce
Parisaheide <parisaheid85@gmail.com>
2021-12-15 17:37:53 +1100

    complete

--- parisa heydarzadeh/wk05 - starts 13th Dec/2-tue/checkpoint/app.rb
+++ parisa heydarzadeh/wk05 - starts 13th Dec/2-tue/checkpoint/app.rb
@@ -4,17 +4,26 @@ def scream (num_input)
     if num_input == 0
       str_array.push("crickets")
       return str_array
-       elsif num_input == 1
-        str_array.push("lol")
-         else num_input > 1
-            while counter < num_input
-             str_array.push("ol")
-             counter = counter + 1;
-            end
-          end
-       end
+    elsif num_input == 1
+     str_array.push("lol")
+    elsif num_input > 1
+        str_array.push("l")   
+      while counter < num_input
+      str_array.push("ol")
+      counter = counter + 1;
+      end
+         
     end
     return str_array.join('');
-  end
+end
  
-print scream(0)
+print scream(4)
+
+# scream(0) #=> "crickets"
+# scream(1) #=> "lol"
+# scream(2) #=> "lolol"
+# scream(3) #=> "lololol"
+# scream(4) #=> "lolololol"
+# scream(5) #=> "lololololol"
+# scream(7) #=> "lololololololol"
+# scream(10) #=> "lolololololololololol"
