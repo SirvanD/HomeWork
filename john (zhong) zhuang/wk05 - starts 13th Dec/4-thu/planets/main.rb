53d84f47e0e662822cc5f9f1564d45aa248cb377
johnzz <zhuangzhong23@gmail.com>
2021-12-16 21:31:01 +1100

    exercise complete

---
+++ john (zhong) zhuang/wk05 - starts 13th Dec/4-thu/planets/main.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pg'
+
+get '/' do
+conn = PG.connect(dbname: 'planets_app')
+  planets = conn.exec("SELECT * FROM planets;")
+conn.close
+
+  erb(:index, locals: {
+    planets: planets
+  })
+end
+
+get '/planets/:id' do
+planet_id = params["id"]
+conn = PG.connect(dbname: 'planets_app')
+  planets = conn.exec("SELECT * FROM planets where id=#{planet_id};")
+  planet = planets[0]
+conn.close
+
+erb(:planet_detail, locals: {
+  planet: planet
+})
+end
+
+
+get '/user_input' do
+erb(:new)
+end
+
+post '/upload' do
+  params.to_s
+  conn = PG.connect(dbname: 'planets_app')
+  conn.exec("INSERT INTO planets (name, image_url, diameter, mass, moon_count) VALUES ('#{params['name']}','#{params['image_url']}', '#{params['diameter']}', '#{params['mass']}', '#{params['moon_count']}');")
+  conn.close
+
+end
