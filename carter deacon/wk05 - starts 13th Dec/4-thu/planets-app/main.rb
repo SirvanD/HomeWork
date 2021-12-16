68b2d42dc05fe6c05187cee0a94316a9da61bdb5
carterdeacon <carter.deacon@gmail.com>
2021-12-16 16:06:09 +1000

    planets task complete

---
+++ carter deacon/wk05 - starts 13th Dec/4-thu/planets-app/main.rb
@@ -0,0 +1,40 @@
+     
+require 'sinatra'
+require 'sinatra/reloader'
+require 'pg'
+
+get '/' do
+  conn = PG.connect(dbname: 'planets_app')
+  sql = "SELECT * FROM planets;"
+  planets = conn.exec(sql)
+conn.close
+  erb :index, locals: {planets: planets}
+end
+
+get '/planets/:id' do
+  planet_id = params['id']
+  conn = PG.connect(dbname: 'planets_app')
+  sql = "SELECT * FROM planets WHERE id = #{planet_id};"
+  result = conn.exec(sql)
+  planet = result[0]
+  conn.close
+  erb :show_planet, locals: {planet: planet}
+end
+
+get '/name_planet' do
+  erb :name_planet
+end
+
+post '/planet_submission' do
+  name = params["name"]
+  image_url = params["image_url"]
+  diameter = params["diameter"]
+  mass = params["mass"]
+  moon_count = params["moon_count"]
+  sql = "INSERT INTO planets (name, image_url, diameter, mass, moon_count) VALUES ('#{name}', '#{image_url}', '#{diameter}', '#{mass}', '#{moon_count}');"
+  conn = PG.connect(dbname: 'planets_app')
+  conn.exec(sql)
+  conn.close
+  redirect '/'
+end
+
