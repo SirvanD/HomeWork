5616cbe935ace9246b8440d11287a3ffd5fb0611
brendonwang9 <brendonwang30@gmail.com>
2021-12-17 16:06:10 +1100

    planetsupdated

--- brendon wang/wk05 - starts 13th Dec/5-fri/planets_app/main.rb
+++ brendon wang/wk05 - starts 13th Dec/5-fri/planets_app/main.rb
@@ -23,6 +23,7 @@ get "/planetdetails/:id" do
   conn = PG.connect(dbname: 'planets_app')
   sql = "select * FROM planets where id = '#{planet_id}';"
   planet = conn.exec(sql)[0]
+  conn.close
   erb :planetdetails, locals: {
     planet: planet,
   }
@@ -37,7 +38,35 @@ post "/addplanet" do
   sql = "INSERT INTO planets (name, image_url, diameter, mass, moon_count) VALUES ('#{planet_name}', '#{planet_image_url}', '#{planet_diameter}', '#{planet_mass}', '#{planet_moon_count}')"
   conn = PG.connect(dbname: 'planets_app')
   conn.exec(sql)
+  conn.close
   redirect "/"
 end
 
+delete "/deleteplanet/:id" do
+  sql = "delete from planets where id = #{params['id']}"
+  conn = PG.connect(dbname: 'planets_app')
+  conn.exec(sql)
+  conn.close
+  redirect "/"
+end
+ 
+get "/editplanet/:id" do
+  sql = "Select * from planets where id = #{params["id"]}"
+  conn = PG.connect(dbname: 'planets_app')
+  planet = conn.exec(sql)[0]
+  conn.close
+  planet
+  erb(:editplanet, locals:{
+    planet: planet,
+  })
+end
+
+put "/updateplanet/:id" do
+  sql = "UPDATE planets set name = '#{params["name"]}', image_url = '#{params["image_url"]}', diameter = #{params["diameter"]}, mass = #{params["mass"]}, moon_count = #{params["moon_count"]} where id = #{params["id"]}"
+  # sql
+  conn = PG.connect(dbname: 'planets_app')
+  conn.exec(sql)
+  conn.close
+  redirect "/"
+end
 
