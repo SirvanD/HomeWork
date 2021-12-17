c2489615b7f8e769df62acbe87a6c8507c3ce08c
brendonwang9 <brendonwang30@gmail.com>
2021-12-17 17:02:03 +1100

    movies_completedatlast

--- brendon wang/wk05 - starts 13th Dec/1-mon/main.rb
+++ brendon wang/wk05 - starts 13th Dec/1-mon/main.rb
@@ -2,8 +2,12 @@ require "sinatra"
 require "sinatra/reloader"
 require "httparty"
 
+$latest_posters = []
+
 get "/" do 
-    erb(:index)
+    erb(:index, locals:{
+        latest_posters: $latest_posters
+    })
 end
 
 get "/about" do 
@@ -14,12 +18,16 @@ get "/search" do
     movie_name = params["movie_name"]
     movie_list_url = "http://omdbapi.com/?s=#{movie_name}=&apikey=9be659e0"
     movie_list = HTTParty.get(movie_list_url)
-    # if movie_list["Search"].length == 1
-    #     get "/moviedata"
-    # else 
-        erb(:moviesearch, locals: {
-        movie_list: movie_list,
+    if movie_list["Search"].length == 1    
+        movie_poster = movie_list["Search"][0]["Poster"]
+        erb(:moviedata, locals: {
+        movie_name: movie_name,
+        movie_poster: movie_poster,
         })
+    else
+    erb(:moviesearch, locals: {
+        movie_list: movie_list,
+    })
     end
 end
 
@@ -28,6 +36,10 @@ get "/moviedata" do
     movie_name_url = "http://omdbapi.com/?t=#{movie_name}=&apikey=9be659e0"
     movie_data = HTTParty.get(movie_name_url)
     movie_poster = movie_data["Poster"]
+    $latest_posters.unshift(movie_poster)
+    if $latest_posters.length > 5
+        $latest_posters.pop()
+    end
     erb(:moviedata, locals: {
         movie_name: movie_name,
         movie_poster: movie_poster,
@@ -35,4 +47,5 @@ get "/moviedata" do
 end
 
 
-# ?movie_name=#{movie_name}
+
+
