bdf167921c671f965613ac4efc5f6bad4d8b2bdf
anguscg <angus.cg@hotmail.com>
2022-01-03 15:34:44 +1100

    added poster and year

--- angus cronin-guss/wk05 - starts 13th Dec/1-mon/movies_2/main.rb
+++ angus cronin-guss/wk05 - starts 13th Dec/1-mon/movies_2/main.rb
@@ -1,10 +1,19 @@
-     
+require 'sinatra/reloader'     
 require 'sinatra'
+require 'httparty'
 
 get '/' do
   erb :index
 end
 
+get "/movies/:title" do 
+  movie_title = params['movie_title']
+  url = "http://www.omdbapi.com/?s=#{movie_title}&apikey=2f6435d9"
+  result = HTTParty.get(url)
+  title = result['Title']
+ 
+  erb :show, locals: {result: result, title: title}
+end
 
 
 
