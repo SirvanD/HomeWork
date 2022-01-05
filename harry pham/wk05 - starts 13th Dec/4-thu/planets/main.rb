e6cb22794d4d3ef5c65f41ec2b13b78c46b21133
HarryP <harrypham__@hotmail.com>
2022-01-05 16:37:33 +1100

    restful conventions

--- harry pham/wk05 - starts 13th Dec/4-thu/planets/main.rb
+++ harry pham/wk05 - starts 13th Dec/4-thu/planets/main.rb
@@ -13,6 +13,11 @@ get '/' do
   }
 end
 
+get '/planets/new' do
+
+  erb :add_planet
+end
+
 get '/planets/:id' do
   planet_id = params['id']
   conn = PG.connect(dbname: 'planets_app')
@@ -25,12 +30,9 @@ get '/planets/:id' do
   }
 end
 
-get '/add_planet' do
 
-  erb :add_planet
-end
 
-post '/input_planet_data' do
+post '/planets' do
   sql = "insert into planets (name, image_url, diameter, mass, moon_count) values ('#{params["name"]}', '#{params["image_url"]}', '#{params["diameter"]}', '#{params["mass"]}', '#{params["moon_count"]}');"
   conn = PG.connect(dbname: 'planets_app')
   conn.exec(sql)
@@ -40,7 +42,7 @@ post '/input_planet_data' do
 
 end
 
-delete '/delete_data' do
+delete '/planets/:id' do
   sql = "delete from planets where id = #{params['planet_id']};"
   conn = PG.connect(dbname: 'planets_app')
   conn.exec(sql)
@@ -49,7 +51,7 @@ delete '/delete_data' do
   redirect "/"
 end
 
-get '/edit_data/:id' do
+get '/planets/:id/edit' do
   sql = "select * from planets where id = #{params['id']};"
   conn = PG.connect(dbname: 'planets_app')
   planet = conn.exec(sql)[0]
@@ -60,7 +62,7 @@ get '/edit_data/:id' do
   }
 end
 
-put '/update_planet_data/:id' do
+put '/planets/:id' do
   sql = "update planets set name = '#{params['name']}', image_url = '#{params['image_url']}', diameter = '#{params['diameter']}', mass = '#{params['mass']}', moon_count = '#{params['moon_count']}' where id = #{params['id']};"
   conn = PG.connect(dbname: 'planets_app')
   planet = conn.exec(sql)
