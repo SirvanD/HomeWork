dc6c40a9d1e53112f4f9a91d26b6048dfd322bbc
dembe7 <moey.ozil@gmail.com>
2022-01-06 16:55:23 +1100

    planets not complete

--- moey (muhammadhasan) al asady/wk04 - starts 6th Dec/5-fri/main.rb
+++ moey (muhammadhasan) al asady/wk04 - starts 6th Dec/5-fri/main.rb
@@ -1,26 +1,53 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'httparty'
+require 'pg'
 
 get '/' do 
     erb(:index, {:layout => :layout})
+end
+
+get '/about' do 
+    erb(:about)
+end
+
+
+get '/movie_list' do
+
+
+    movie_search = params["movie_search"]
+    url = "https://www.omdbapi.com/?s=#{movie_search}&apikey=6139fe0d"
+    result = HTTParty.get(url)
+    title = result["Title"],
+    movie_search = result["Search"],
+    search = movie_search[0]
+
+    erb(:movie_list, locals: {
+        movie_list: movie_search,
+        movie_title: title,
+        search: search,
+        title: title
+        
+        
+    })
+
+
+
 end
 
 
 get '/movie' do
 
 
-    movie_title = params["ticker"]
+    movie_title = params["movie_input"]
     url = "http://omdbapi.com/?t=#{movie_title}&apikey=2f6435d9"
 
     res = HTTParty.get(url)
-    puts res
-
+    
     erb(:movie, locals: {
         title: res["Title"],
         year: res["Year"],
         poster: res["Poster"]
-        # title: Title,
 
     })
 
@@ -29,13 +56,6 @@ get '/movie' do
     # puts res["Year"]
 end
 
-get '/list' do 
 
-    rb(:list, locals: {
-        title: res["Title"],
-        
-
-    })
-end
 
 
