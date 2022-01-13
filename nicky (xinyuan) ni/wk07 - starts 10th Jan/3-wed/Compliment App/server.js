093eff8266692bf51f73ec46c81d8ba31c0233e2
nicky <nixinyuan0925@gmail.com>
2022-01-13 17:14:48 +1100

    css not added yet

---
+++ nicky (xinyuan) ni/wk07 - starts 10th Jan/3-wed/Compliment App/server.js
@@ -0,0 +1,30 @@
+
+  
+// colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+
+const express = require('express')
+
+const app = express()
+
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
+
+
+app.get('/', function(req, res) {
+    var compliments = [
+        "Are you a parking ticket? Because you’ve got FINE written all over you.",
+        "You might be the primary reason for global warming.",
+        "You must be the square root of negative one... Because you are unreal.",
+        "If you are here, who's running heaven?",
+        "So, aside from taking my breath away, what do you do for a living?",
+        "I can't tell if that was an earthquake, or if you just seriously rocked my world."
+    ]
+    var compliment = compliments[Math.floor(Math.random() * 4)];
+  
+    res.render('show', {
+      compliment: compliment
+    });
+});
+
+app.listen(8080)
