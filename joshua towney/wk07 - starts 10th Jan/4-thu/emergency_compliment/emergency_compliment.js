c2a262b1c8c561a543505cf6504bcf56a6342c94
Joshua Towney <joshtowney@hotmail.com>
2022-01-14 17:47:11 +1100

    citypix hw complete

--- joshua towney/wk07 - starts 10th Jan/4-thu/emergency_compliment/emergency_compliment.js
+++ joshua towney/wk07 - starts 10th Jan/4-thu/emergency_compliment/emergency_compliment.js
@@ -1,19 +1,48 @@
 const express = require('express')
-const res = require('express/lib/response')
+// const res = require('express/lib/response') - this is a gues from vs code so is uneeded.
 const dayjs = require('dayjs')
 const _ = require('underscore')
+// const bodyParser = require('body-parser')
 
 const app = express()
 const now = dayjs()
 
+// REQUESTS - can get extra info via:
+// url -query strings
+// url - named paramter (pretty urls)
+// form - get - query string
+// form - post - body
 
+// res.query
+// req.params
+// req.body
+
+
+// function logging(req, res, next) {
+//     console.log(`${req.method} ${req.path} ${new Date()}`)
+//     next()
+// }
+
+// ---+
+//    |
+//    V
+
+// app.use(logging) - all requests will go through logging first - useful to display timestamps and where they tried to go etc.
+
+// ---+
+//    |
+//    V stops at station - keeps going til it gets a response. if o response 404 error
 
 app.set('view engine', 'ejs')
 
-app.set('views', 'templates')
+app.set('views', 'templates') // or './views' instead of templates
+
+// this is an example of a middleware -  function
+// app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.send(now.toString())
+    //res.send(new Date().toLocaleTimeString) - negates time library
 })
 
 app.get('/compliment', (req, res) => {
@@ -28,15 +57,26 @@ app.get('/compliment', (req, res) => {
 
     const colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
 
+    // let randomCompliment = _.sample(compliment)
+    // let randomColor = _.sample(colors)
+
     res.render('show', {colors, _ ,compliments})
+                       //{compliment: randomCompliment}
+                       //{color: randomColor}
+                       // key vlue pairs like an erb file
 
 })
 
-app.get('/game/', (req, res) => {
+// app.get('/hello, ()req, res) => {
+//     res.send(req.body.name) - works with middleware to get data from form - submit the form using post. name = 'name
+// })
+
+app.get('/game', (req, res) => {
 
     const hands = ['rock','paper','scissors']
     let playerHand = req.query.hand.toLowerCase()
     let serverHand = _.sample(hands)
+
     if (playerHand != 'rock' && playerHand != 'paper' && playerHand != 'scissors') {
         res.send("That's not a valid option, idiot!")
     } else if (playerHand == serverHand) {
---
+++ joshua towney/wk07 - starts 10th Jan/5-fri/city_pix/citypix/main.js
@@ -0,0 +1,25 @@
+
+const cities = ['NYC', 'SF', 'LA', 'SYD', 'ATX'];
+const cityPics = {
+    'Select a City': 'citipix_skyline.jpg',
+    'NYC': 'nyc.jpg',
+    'SF': 'sf.jpg',
+    'LA': 'la.jpg',
+    'SYD': 'sydney.jpg',
+    'ATX': 'austin.jpg' 
+};
+var dropdown = document.querySelector('#city-type');
+
+cities.forEach(city => {
+    var option = document.createElement('option');
+    option.text = city; // create element 'option' with a value of the city in the active loop
+    dropdown.add(option);
+});
+
+var form = document.querySelector('form');
+var selectedCity = '';
+
+form.addEventListener('change', () => {
+    selectedCity = document.querySelector('#city-type').selectedOptions[0].value; //selected item in dropdown always in position 0
+    document.querySelector('body').style.backgroundImage = `url("images/${cityPics[selectedCity]}")`;
+});
