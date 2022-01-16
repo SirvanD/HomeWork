90657ec77ae6c74ceb865cba9bea2f54ee3e22cf
anguscg <angus.cg@hotmail.com>
2022-01-16 19:45:05 +1100

    Code from DTs HW Session

--- angus cronin-guss/wk07 - starts 10th Jan/3-wed/emergency-compliment/server.js
+++ angus cronin-guss/wk07 - starts 10th Jan/3-wed/emergency-compliment/server.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require ('express')
+const bodyParser = require('body-parser')
 const _ = require('underscore')
-require('nodemon')
-const app  = express() 
+const app = express()
+const PORT = 8080
 
 app.set('view engine', 'ejs')
 app.set('views', './templates')
@@ -12,37 +13,97 @@ app.get('/', (req,res) => {
 })
 
 
+function logging(req, res, next) {
+    console.log(`${req.method} ${req.path} ${new Date()}`);
+    next()
+    
+}
 
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(logging)
+
+// Task 1 
+app.get('/', (req, res) => {
+    res.send(new Date().toLocaleDateString())
+})
+
+// Task 2
 app.get('/compliment', (req, res) => {
-    
-    
-    compliments = [
+
+    const compliments = [
         "Your instructors love you",
         "High five = ^5",
         "Shut up and take my money",
         "It's almost beer o'clock",
         "The Force is strong with you"
       ]
-      
-      colors = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+    const color = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+    let randomColor = _.sample(color)
 
-      let randomComp = res.send(_.sample(compliments))
-      let randomColor = res.send(_.sample(colors))
-      
-      res.render('compliments')
+    let randomCompliment = _.sample(compliments)
+    // needs to go last 
+    res.render('compliments', {
+        compliment: randomCompliment, 
+        color: randomColor
+    }) 
+
     
-} )
+})
 
-app.get('/game/:option', (req, res) => { 
+// Task 3
+
+app.get('/game', (req, res) => {
+
     let hand = [
         'rock',
         'paper',
         'scissors'
     ]
-    res.send(req.params.option)
-    let computer = req.get(_.sample(hand))
-    res.send(computer)
+    let userHand = req.query.hand
+    let computerHand = _.sample(hand)
+
+    res.send(`User hand ${userHand}, computer hand ${computerHand}`)
+    if (userHand == computerHand) {
+        res.send(`It's a draw`)
+    } else if ( userHand == 'rock' && computerHand == 'scissors') {
+        res.send('The user who picked rock wins')
+    } else if ( userHand == 'paper' && computerHand == 'rock') {
+        res.send('The user who picked paper wins')
+    } else if ( userHand == 'scissors' && computerHand == 'rock') {
+        res.send('The user who picked rock wins')
+    }
+})
+
+// Task 4
+
+app.get('/:name', (req, res) => {
+
+    const compliments = [
+        "Your instructors love you",
+        "High five = ^5",
+        "Shut up and take my money",
+        "It's almost beer o'clock",
+        "The Force is strong with you"
+      ]
+    const color = ["#FFBF00", "#0080FF","#01DF3A","#FF0080"]
+    let randomColor = _.sample(color)
+    let name = req.params.name
+    let randomCompliment = _.sample(compliments)
+    // needs to go last 
+    res.render('task4', {
+        compliment: randomCompliment, 
+        color: randomColor,
+        name: name
+    }) 
+    
+
+    
 })
 
 
-app.listen(1290) 
+
+app.listen(PORT, () => { 
+    console.log(`Express has taken the stage at ${PORT}`);
+    
+})
