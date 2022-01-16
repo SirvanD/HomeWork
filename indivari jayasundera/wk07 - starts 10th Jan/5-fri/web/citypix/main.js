f619d06b0d4a671e85b566bcb68f9bccae2d6419
indivari <indivarij@gmail.com>
2022-01-16 17:50:02 +1100

    select options are working

---
+++ indivari jayasundera/wk07 - starts 10th Jan/5-fri/web/citypix/main.js
@@ -0,0 +1,28 @@
+var city = document.getElementById("city-type");
+
+// var city = document.querySelector("select");
+
+var option1 = document.createElement("option");
+option1.value = "NYC";
+option1.text = "NYC";
+city.options.add(option1);
+
+var option2 = document.createElement("option");
+option2.text = "SF";
+option2.value = "SF";
+city.options.add(option2);
+
+var option3 = document.createElement("option");
+option3.value = "LA";
+option3.text = "LA";
+city.appendChild(option3);
+
+var option4 = document.createElement("option");
+option4.value = "SYD";
+option4.text = "SYD";
+city.appendChild(option4);
+
+var option5 = document.createElement("option");
+option5.value = "ATX";
+option5.text = "ATX";
+city.appendChild(option5);
