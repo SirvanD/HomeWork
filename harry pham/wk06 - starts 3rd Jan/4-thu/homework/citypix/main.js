7ecd8739fa358cc6468d82c16b0239d089025827
HarryP <harrypham__@hotmail.com>
2022-01-16 18:23:55 +1100

    homework - city not completed

--- harry pham/wk06 - starts 3rd Jan/4-thu/homework/citypix/main.js
+++ harry pham/wk06 - starts 3rd Jan/4-thu/homework/citypix/main.js
@@ -1,43 +1,38 @@
-const select = document.querySelector('select')
+const select1 = document.querySelector('select')
 const form = document.querySelector('form')
 const body = document.querySelector('body')
 const options = document.querySelector('select option')
-const nyc = document.createElement('option')
-const sf = document.createElement('option')
-const la = document.createElement('option')
-const austin = document.createElement('option')
-const sydney = document.createElement('option')
-
 
 const cities = ['New York City', 'San Francisco', 'Los Angelos', 'Austin', 'Sydney']
 const cityImg = ['nyc', 'sf', 'la', 'austin', 'sydney']
 
+let i=0
 cities.forEach(city => {
+
     const option = document.createElement('option')
     option.textContent = city
-    select.appendChild(option)
+    option.className = cityImg[i]
+    select1.appendChild(option)
+    i++
+    console.log(i)
 })
 
-// nyc.textContent = cities[0]
-// select.appendChild(nyc)
+const nycElement = document.querySelector('.nyc')
+const sfElement = document.querySelector('.sf')
+const laElement = document.querySelector('.la')
+const austinElement = document.querySelector('.austin')
+const sydneyElement = document.querySelector('.sydeny')
 
-// sf.textContent = cities[1]
-// select.appendChild(sf)
+document.querySelector("#city-type > option.sf")
+function handleBg() {
+    body.className="nyc"
+}
 
-// la.textContent = cities[2]
-// select.appendChild(la)
-
-// austin.textContent = cities[3]
-// select.appendChild(austin)
-
-// sydney.textContent = cities[4]
-// select.appendChild(sydney)
-
-// body.style.backgroundImage = `url(./images/nyc.jpg)`
-
+select1.addEventListener('click', handleBg)
 
 const btn = document.createElement('button')
 btn.textContent = 'select'
 btn.id = 'submit-btn'
+
 form.appendChild(btn)
 
---
+++ harry pham/wk06 - starts 3rd Jan/4-thu/homework/homework.js
@@ -0,0 +1,105 @@
+// .forEach() and .filter()
+// Write a function named onlyEven that returns evens numbers in an array. Write one version that use forEach and then write another version that uses filter
+
+// onlyEven([1,2,3,4,5,6,7]) // => [2,4,6]
+
+// function onlyEven(nums) {
+//     let even = []
+//     console.log(nums)
+//     nums.forEach(num => {
+//         console.log(num)
+//         if(num % 2 == 0) {
+//             even.push(num)
+//         }
+//     })
+//     return even
+// }
+
+// function onlyEven(nums) {
+//     console.log(nums)
+//     const even = nums.filter(num => num % 2 === 0)
+//     return even
+// }
+
+
+// .forEach() or .filter()
+// countIntegers – counts how many integers there are in an array
+// countIntegers([4, 2.2, 5, 6, 4.2, 8.2, 4]) // => 4
+// Hint: Google how you can check if a number is an integer in JavaScript
+
+// function countIntegers(nums) {
+//     let integers = []
+//     console.log(nums)    
+//     nums.forEach(num => {
+//         if(Number.isInteger(num)) {
+//             integers.push(num)      
+//         }
+//     })
+//     return integers
+// }
+
+// function countIntegers(nums) {
+//     console.log(nums)
+//     const integers = nums.filter(num => Number.isInteger(num))
+//     console.log(integers)
+//     return integers
+// }
+
+// countIntegers([4, 2.2, 5, 6, 4.2, 8.2, 4])
+
+// .map() oldie but goodie
+// Write a function lengths that accepts a single parameter as an argument, namely an array of strings. The function should return an array of numbers where each number is the length of the corresponding string. use .map
+
+// lengths(['my', 'cake', 'pudding']); // => [2,4,7]
+
+// function mapping(strs) {
+//     console.log(strs)
+//     const lengthOfStr = strs.map(str => str.length)
+//     console.log(lengthOfStr)
+// }
+
+// mapping(['my', 'cake', 'pudding'])
+
+
+
+// .map()
+// getSquares – takes in an array of numbers and returns an array of their squares. If you want more practice write one version using .forEach and then write another version using .map
+// getSquares([1, 2, 3, 4, 5]) // => [1, 4, 9, 16, 25]
+
+    function getSquares(nums) {
+        console.log(nums.map(num => num * num))
+        return nums.map(num => num * num)
+    }
+
+    getSquares([1, 2, 3, 4, 5])
+
+// .map(), .filter() & .reduce()
+// Write an expression using array iterator methods (like filter and reduce) to compute the total value of the machines in the inventory array. Use arrow functions.
+var inventory = [
+  {type:   "machine", value: 5000},
+  {type:   "machine", value:  650},
+  {type:      "duck", value:   10},
+  {type: "furniture", value: 1200},
+  {type:   "machine", value:   77}
+]
+
+// function totalValue(items) {
+//     const valueOfInv = items.map(item => item.value)
+//     const sum = valueOfInv.reduce((accum, curr) => {
+//         return accum + curr
+//     }, 0)
+//     console.log(sum)
+// }
+
+// totalValue(inventory)
+
+// Get an array of all the machines with value over 500
+
+// function conditionArray(items) {
+//     console.log(items)
+//     const conditionValue = items.filter(item => item.value > 500)
+//     console.log(conditionValue)
+// }
+
+// conditionArray(inventory)
+
