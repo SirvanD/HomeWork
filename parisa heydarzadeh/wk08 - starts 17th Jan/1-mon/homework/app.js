b3b91c5d9709b3330fca777c9cab85f721a28c3f
Parisaheide <parisaheid85@gmail.com>
2022-01-18 16:15:05 +1100

    done

--- parisa heydarzadeh/wk08 - starts 17th Jan/1-mon/homework/app.js
+++ parisa heydarzadeh/wk08 - starts 17th Jan/1-mon/homework/app.js
@@ -11,26 +11,26 @@ var changebrushBox = function (event) {
 brushBtn.addEventListener("click", changebrushBox);
 
 // step 2
-var numOfPixels = 2000;
-var checkBox = document.querySelector("#pixel");
+var numOfPixels = 2050;
+var canvas = document.querySelector("#canvas");
 
 for (var i = 0; i < numOfPixels; i++) {
   var elem = document.createElement("div");
-  elem.classList.add("pixel");
-  checkBox.appendChild(elem);
+  elem.classList.add("canvas");
+  canvas.appendChild(elem);
 }
 
 // step 3 - 4 - 5 - 6
-var pixels = document.querySelector("#pixel");
+var pixels = document.querySelector("#canvas");
 
 // got to the parent and add event listener
 var changePixelColor = function (event) {
-  if (event.target.classList.contains("pixel")) {
+  if (event.target.classList.contains("canvas")) {
     event.target.style.backgroundColor = color.value;
   }
 };
 
-pixels.addEventListener("mouseover", changePixelColor);
+pixels.addEventListener("click", changePixelColor);
 
 // step 7
 var movieBtn = document.querySelector("#movie");
@@ -38,10 +38,11 @@ var movieName = document.querySelector(".movieName");
 
 movieBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  console.log(`aahaaha`, aahaaha);
-  // axios
-  //   .get(`https://www.omdbapi.com/?t=${movieName.value}&apikey=2f6435d9`)
-  //   .then((res) => {
-  //     checkBox.style.backgroundImage = `url(${res.Poster})`;
-  //   });
+
+  axios
+    .get(`https://omdbapi.com/?t=${movieName.value}&apikey=8f66dc3c`)
+    .then((res) => {
+      console.log(res.data.Poster);
+      canvas.style.backgroundImage = `url(${res.data.Poster})`;
+    });
 });
