a4d83153ec24c841792e9dbd0d130da7b649d2f8
indivari <indivarij@gmail.com>
2022-01-18 16:04:37 +1100

    code modified to clear previous search results with each new search

--- indivari jayasundera/wk08 - starts 17th Jan/2-tue/giphy/app.js
+++ indivari jayasundera/wk08 - starts 17th Jan/2-tue/giphy/app.js
@@ -2,23 +2,23 @@
 
 var btnSearch = document.querySelector(".search-btn");
 var searchInput = document.querySelector("input");
+var imgContainer = document.querySelector(".image-container");
 
 // var btnSearch = document.getElementById("search-btn");
 
 const getImages = () => {
   let searchKey = searchInput.value;
+  imgContainer.innerHTML = "";
   axios
     .get(
       `https://api.giphy.com/v1/gifs/search?q=${searchKey}&api_key=YuprO7hy60upsPqQuKdWKlJsN3HXe7nt&q=cats&limit=25&offset=0&rating=g&lang=en`
     )
     .then((res) => {
-      console.log(res);
-
       for (var i = 0; i < 10; i++) {
         var giphyURL = res.data.data[i].images.fixed_height.url;
         var newImage = document.createElement("img");
         newImage.src = giphyURL;
-        document.body.appendChild(newImage);
+        imgContainer.appendChild(newImage);
       }
     });
 };
