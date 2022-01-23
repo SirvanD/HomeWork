9d7d480ce371b2b740f6813e38a13f4ed19a65d6
anguscg <angus.cg@hotmail.com>
2022-01-23 20:09:00 +1100

    Sorting practise HW complete

--- angus cronin-guss/wk08 - starts 17th Jan/4-thu/sort-practise.js
+++ angus cronin-guss/wk08 - starts 17th Jan/4-thu/sort-practise.js
@@ -48,3 +48,45 @@ var horses = [
       daysWorked: 515    
     }  
   ]
+  // sort the horses by year of birth from youngest to oldest
+
+  const sortHorses = horses.sort((a,b) => {
+    if (b.yearOfBirth < a.yearOfBirth ) {
+      return -1
+    }
+  })
+
+
+  var items = [
+    { name: 'apple', type: 'fruit', full_price: 2, discount: 0 },
+    { name: 'potato', type: 'vegie', full_price: 5, discount: 0 },  
+    { name: 'banana', type: 'fruit', full_price:4, discount: 1 },
+    { name: 'orange', type: 'fruit', full_price: 3, discount: 2 },
+    { name: 'brocoli', type: 'vegie', full_price: 2, discount: 2 },
+  ]
+
+// sort items by full price from cheapest to most expensive
+
+const sortPrices = items.sort((a,b) => {
+  if (b.full_price - b.discount < a.full_price - a.discount) {
+    return -1
+  }
+})
+
+// sort items by name in the ascending
+
+const sortByName = items.sort((a,b) => {
+  if (b.name > a.name) {
+    return - 1
+  }
+})
+
+var products = [
+  [5, 'kiwi'],
+  [3, 'potato'],
+  [7, 'apple']
+ ]
+// sort products by name in descending order
+const sortProducts = products.sort((a,b) => {
+  return a[1] - b[1]
+})
