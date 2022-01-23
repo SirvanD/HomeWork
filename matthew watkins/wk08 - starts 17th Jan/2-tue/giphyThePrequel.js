43974ebd032a5117796af33df7772665679b3bd4
Matthew <mattwatkins000@hotmail.com>
2022-01-23 12:14:45 +1100

    updated giphy

--- matthew watkins/wk08 - starts 17th Jan/2-tue/giphyThePrequel.js
+++ matthew watkins/wk08 - starts 17th Jan/2-tue/giphyThePrequel.js
@@ -13,22 +13,23 @@ var button = document.querySelector('button')
 
 
 button.addEventListener('click', () => {
-    // event.preventDefault()
-    console.log(`click`)
-    console.log(searchTerm)
-    
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=YmeVNpMPCMenN19rAQxFpn6vj2JLKFP2&limit=10`).then(handleResponse)
-    var imgSrc = document.createElement('img')
-    body.appendChild(imgSrc)
-    imgSrc.src = `${gifUrl}`
-    function handleResponse(res) {
-        console.log(res.data)
-        
-        res.data.data.forEach(data => {
-            var gifUrl = data.images.original.url
-            
-        });
-        
-    }
-})
-//data[0].images.original.url
+        event.preventDefault()
+        console.log(`click`)
+        console.log(searchTerm)
+
+        axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=YmeVNpMPCMenN19rAQxFpn6vj2JLKFP2&limit=10`).then(handleResponse)
+        var imgSrc = document.createElement('img')
+        body.appendChild(imgSrc)
+        imgSrc.src = `${gifUrl}`
+
+        function handleResponse(res) {
+            console.log(res.data)
+
+            res.data.data.forEach(data => {
+                var gifUrl = data.images.original.url
+
+            });
+
+        }
+    })
+    //data[0].images.original.url
